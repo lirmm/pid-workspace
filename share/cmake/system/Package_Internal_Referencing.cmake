7268b4534a96f539ac71a030dab251e01ad2a8ad
Robin Passama <passama@lirmm.fr>
2013-10-02 18:55:03 +0200

    git use for retrieving versions is coded

--- share/cmake/system/Package_Internal_Referencing.cmake
+++ share/cmake/system/Package_Internal_Referencing.cmake
@@ -73,6 +73,39 @@ else()
 endif()
 endfunction(need_Install_Packages)
 
+###
+function(resolve_Required_Package_Version version_possible min_version is_exact package)
+
+foreach(version IN ITEMS ${${PROJECT_NAME}_TOINSTALL_${package}_VERSIONS})
+	get_Version_String_Numbers("${version}.0" compare_major compare_minor compared_patch)
+	if(NOT MAJOR_RESOLVED)#first time
+		set(MAJOR_RESOLVED ${compare_major})
+		set(CUR_MINOR_RESOLVED ${compare_minor})
+		if(${PROJECT_NAME}_TOINSTALL_${package}_${version}_EXACT)
+			set(CURR_EXACT TRUE)
+		else()
+			set(CURR_EXACT FALSE)
+		endif()
+	elseif(NOT compare_major EQUAL ${MAJOR_RESOLVED})
+		set(${version_possible} FALSE PARENT_SCOPE)
+		return()
+	elseif(CURR_EXACT AND (compare_minor GREATER ${CUR_MINOR_RESOLVED}))
+		set(${version_possible} FALSE PARENT_SCOPE)
+		return()
+	elseif(NOT CURR_EXACT AND (compare_minor GREATER ${CUR_MINOR_RESOLVED}))
+		set(CUR_MINOR_RESOLVED ${compare_minor})
+		if(${PROJECT_NAME}_TOINSTALL_${package}_${version}_EXACT)
+			set(CURR_EXACT TRUE)
+		else()
+			set(CURR_EXACT FALSE)
+		endif()
+	endif()
+endforeach()
+set(${version_possible} TRUE PARENT_SCOPE)
+set(${min_version} "${MAJOR_RESOLVED}.${CUR_MINOR_RESOLVED}" PARENT_SCOPE)
+set(${is_exact} ${CURR_EXACT} PARENT_SCOPE)
+endfunction(resolve_Required_Package_Version)
+
 
 ### root function for launching automatic installation process
 function(install_Required_Packages INSTALLED_PACKAGES)
@@ -135,6 +168,7 @@ package_Source_Exists_In_Workspace(IS_EXISTING PATH_TO_SOURCE ${package})
 if(IS_EXISTING)
 	set(USE_SOURCES TRUE)
 else()
+	set(IS_EXISTING)
 	package_Reference_Exists_In_Workspace(IS_EXISTING ${package})
 	if(IS_EXISTING)
 		set(USE_SOURCES FALSE)
@@ -143,42 +177,224 @@ else()
 		return()
 	endif()
 endif()
+if(${PROJECT_NAME}_TOINSTALL_${package}_VERSIONS)
 # 1) resolve finally required package version (if any specific version required) (major.minor only, patch is let undefined)
-set(POSSIBLE)
-set(VERSION_MIN)
-set(EXACT)
-resolve_Required_Package_Version(POSSIBLE VERSION_MIN EXACT ${package})
-if(NOT POSSIBLE)
-	message(SEND_ERROR "Install : impossible to find an adequate version for package ${package}")
+	set(POSSIBLE)
+	set(VERSION_MIN)
+	set(EXACT)
+	resolve_Required_Package_Version(POSSIBLE VERSION_MIN EXACT ${package})
+	if(NOT POSSIBLE)
+		message(SEND_ERROR "Install : impossible to find an adequate version for package ${package}")
+		return()
+	endif()
+	set(NO_VERSION FALSE)	
+else()
+	set(NO_VERSION TRUE)
 endif()
 
-if(USE_SOURCES)
-# go to package source and find all version matching the pattern of VERSION_MIN : if exact taking VERSION_MIN, otherwise taking the gretest version number 
-
-# taking the last patch version available for this major.minor
-
-# now go to the git tag vmajor.minor.patch (git checkout)
-
-# build and install the version (cd build > cmake .. -D... > make build) usig cmake -E chdir command
-
+if(USE_SOURCES) #package sources reside in the workspace
+	if(NOT NO_VERSION)
+		deploy_Source_Package_Version(${package} ${VERSION_MIN} ${EXACT})
+	else()
+		deploy_Source_Package(${package}) # case when the sources exist but haven't been installed yet (should never happen)
+	endif()
 else()#using references
 include(Refer${package}.cmake)
-# listing available binaries of the package and searching if there is any "good version" regarding the pattern VERSION_MIN : if exact taking VERSION_MIN, otherwise taking the gretest version number 
+# listing available binaries of the package and searching if there is any "good version" regarding the pattern VERSION_MIN : if exact taking VERSION_MIN, otherwise taking the greatest version number 
+set(PACKAGE_BINARY_DEPLOYED)
+if(NOT NO_VERSION)
+	deploy_Binary_Package_Version(PACKAGE_BINARY_DEPLOYED ${package} ${VERSION_MIN} ${EXACT})
+else()
+	deploy_Binary_Package(PACKAGE_BINARY_DEPLOYED ${package})
+endif()
+if(PACKAGE_BINARY_DEPLOYED) # if there is ONE adequate reference, downloading and installing it
+	set(${INSTALL_OK} TRUE PARENT_SCOPE)
+else()# otherwise, trying to "git clone" the package source (if it can be accessed)
+set(DEPLOYED)
+deploy_Package_Repository(DEPLOYED ${package})
+if(DEPLOYED) # doing the same as for the USE_SOURCES step
+	if(NOT NO_VERSION)
+		deploy_Source_Package_Version(SOURCE_DEPLOYED ${package} ${VERSION_MIN} ${EXACT})
+	else()
+		deploy_Source_Package(SOURCE_DEPLOYED ${package})
+	endif()
+	if(NOT SOURCE_DEPLOYED)
+		message(SEND_ERROR "Install : impossible to build the package sources ${package}. Try \"by hand\".")
+	endif()
+else()
+	message(SEND_ERROR "Install : impossible to locate source repository of package ${package}")
+endif()
 
-# if there is ONE, downloading and installing it
-
-# otherwise, trying to "git clone" the package source (if it can be accessed)
-# doing the same as for the USE_SOURCES step
-	
 endif()
 
 endfunction(install_Package)
 
+
 ###
-function(resolve_Required_Package_Version version_possible min_version is_exact package)
+function(deploy_Package_Repository IS_DEPLOYED package)
+if(${package}_ADDRESS)
+	execute_process(${CMAKE_COMMAND} -E chdir ${WORKSPACE_DIR}/packages git clone ${${package}_ADDRESS} OUTPUT_QUIET ERROR_QUIET)
+	if(EXISTS ${WORKSPACE_DIR}/packages/${package} AND IS_DIRECTORY ${WORKSPACE_DIR}/packages/${package})
+		set(${IS_DEPLOYED} TRUE PARENT_SCOPE)
+	else()
+		set(${IS_DEPLOYED} FALSE PARENT_SCOPE)
+		message(SEND_ERROR "Install : impossible to clone the repository of package ${package} (bad repository address or you have no clone rights for this repository)")
+	endif()
+else()
+	set(${IS_DEPLOYED} FALSE PARENT_SCOPE)
+	message(SEND_ERROR "Install : impossible to clone the repository of package ${package} (no repository address defined)")
+endif()
+endfunction(deploy_Package_Repository)
 
 
+### TODO
+function(deploy_Binary_Package package)
 
 
-endfunction(resolve_Required_Package_Version)
+endfunction(deploy_Binary_Package)
+
+
+### TODO
+function(deploy_Binary_Package_Version package VERSION_MIN EXACT)
+
+
+endfunction(deploy_Binary_Package_Version)
+
+
+### 
+function(deploy_Source_Package DEPLOYED package)
+	set(ERROR_OCCURRED)
+	execute_process(
+		COMMAND ${CMAKE_COMMAND} .. -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_WITH_PRINT_MESSAGES:BOOL=OFF -DUSE_LOCAL_DEPLOYMENT:BOOL=OFF -DGENERATE_INSTALLER:BOOL=OFF -DBUILD_LATEX_API_DOC:BOOL=OFF -DBUILD_AND_RUN_TESTS:BOOL=OFF -DBUILD_PACKAGE_REFERENCE:BOOL=OFF -DREQUIRED_PACKAGES_AUTOMATIC_DOWNLOAD:BOOL=ON
+		COMMAND ${CMAKE_BUILD_TOOL} build
+		WORKING_DIRECTORY ${WORKSPACE_DIR}/packages/${package}/build
+		ERROR_VARIABLE ERROR_OCCURRED
+		OUTPUT_QUIET)
+
+	if(ERROR_OCCURRED)
+		set(${DEPLOYED} FALSE PARENT_SCOPE)
+		return()
+	endif()
+	set(${DEPLOYED} TRUE PARENT_SCOPE)
+endfunction(deploy_Source_Package)
+
+
+###
+function(deploy_Source_Package_Version DEPLOYED package VERSION_MIN EXACT)
+
+# go to package source and find all version matching the pattern of VERSION_MIN : if exact taking VERSION_MIN, otherwise taking the greatest version number 
+execute_process(
+		COMMAND git tag -l 'v*'
+		WORKING_DIRECTORY ${WORKSPACE_DIR}/packages/${package}
+		OUTPUT_VARIABLE res
+		)
+if(NOT res) #no version available => BUG
+	set(${DEPLOYED} FALSE PARENT_SCOPE)
+	return()
+endif()
+string(REGEX REPLACE "^([0-9]+)\.([0-9]+)$)" "\\1;\\2" REFVNUMBERS ${VERSION_MIN})
+list(GET REFVNUMBERS 0 ref_major)
+list(GET REFVNUMBERS 1 ref_minor)
+
+set(ALL_IS_OK)
+if(EXACT)
+	set(curr_max_patch_number -1)
+	foreach(version IN ITEMS ${res})
+		set(VNUMBERS)
+		string(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)\\.([0-9]+)$" "\\1;\\2;\\3" VNUMBERS ${version})
+		if(NOT "${version}" STREQUAL "${VNUMBERS}")#i.e. match found (this was a well formed version number)
+			list(GET VNUMBERS 0 compare_major)
+			list(GET VNUMBERS 1 compare_minor)
+			list(GET VNUMBERS 2 compare_patch)
+			if(	${compare_major} EQUAL ${ref_major} 
+				AND ${compare_minor} EQUAL ${ref_minor} 
+				AND ${compare_patch} GREATER ${curr_max_patch_number})
+				set(curr_max_patch_number ${compare_patch})# taking the last patch version available for this major.minor
+			endif()
+		endif()
+	endforeach()
+	if(NOT curr_max_patch_number EQUAL -1)#i.e. nothing found
+		set(${DEPLOYED} FALSE PARENT_SCOPE)
+		return()
+	endif()
+	
+	build_And_Install_Package(ALL_IS_OK ${package} "${ref_major}.${ref_minor}.${curr_max_patch_number}")
+	
+else()
+	set(curr_max_patch_number -1)
+	set(curr_max_minor_number ${ref_minor})
+	foreach(version IN ITEMS ${res})
+		set(VNUMBERS)
+		string(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)\\.([0-9]+)$" "\\1;\\2;\\3" VNUMBERS ${version})
+		if(NOT "${version}" STREQUAL "${VNUMBERS}")#i.e. match found (this was a well formed version number)
+			list(GET VNUMBERS 0 compare_major)
+			list(GET VNUMBERS 1 compare_minor)
+			list(GET VNUMBERS 2 compare_patch)
+			if(${compare_major} EQUAL ${ref_major})
+				if(	${compare_minor} EQUAL ${curr_max_minor_number} 
+					AND ${compare_patch} GREATER ${curr_max_patch_number})
+					set(curr_max_patch_number ${compare_patch})# taking the newest patch version for the current major.minor
+				elseif(${compare_minor} GREATER ${curr_max_minor_number} )
+					set(curr_max_patch_number ${compare_patch})# taking the patch version of this major.minor
+					set(curr_max_minor_number ${compare_minor})# taking the last minor version available for this major
+				endif()
+				
+			endif()
+		endif()
+	endforeach()
+	if(NOT curr_max_patch_number EQUAL -1)#i.e. nothing found
+		set(${DEPLOYED} FALSE PARENT_SCOPE)
+		return()
+	endif()
+	
+	build_And_Install_Package(ALL_IS_OK ${package} "${ref_major}.${curr_max_minor_number}.${curr_max_patch_number}")
+endif()
+
+if(ALL_IS_OK)
+	set(${DEPLOYED} TRUE PARENT_SCOPE)
+else()
+	set(${DEPLOYED} FALSE PARENT_SCOPE)
+endif()
+
+# now go to the git tag vmajor.minor.patch (git checkout)
+
+# build and install the version (cd build > cmake .. -D... > make build) usig cmake -E chdir command
+endfunction(deploy_Source_Package_Version)
+
+function(build_And_Install_Package DEPLOYED package version)
+
+# 0) memorizing the current branc the user is working with
+execute_process(COMMAND git branch
+		WORKING_DIRECTORY ${WORKSPACE_DIR}/packages/${package}
+		OUTPUT_VARIABLE current_branches ERROR_QUIET)
+
+foreach(branch IN ITEMS ${current_branches})
+	string(REGEX REPLACE "^\\* (.*)$" "\\1" A_BRANCH ${branch})
+	if(NOT "${branch}" STREQUAL "${A_BRANCH}")#i.e. match found (this is the current branch)
+		set(curr_branch ${A_BRANCH})
+		break()
+	endif()
+endforeach()
+
+# 1) going to the adequate git tag matching the selected version
+execute_process(COMMAND git checkout master
+		COMMAND git checkout tags/v${version}
+		WORKING_DIRECTORY ${WORKSPACE_DIR}/packages/${package}
+		OUTPUT_QUIET ERROR_QUIET)
+# 2) building sources
+set(IS_BUILT)
+deploy_Source_Package(IS_BUILT ${package})
+
+# 3) going back to the initial branch in use
+execute_process(COMMAND git checkout ${curr_branch}
+		WORKING_DIRECTORY ${WORKSPACE_DIR}/packages/${package}
+		OUTPUT_QUIET ERROR_QUIET)
+
+if(IS_BUILT)
+	set(${DEPLOYED} TRUE PARENT_SCOPE)
+else()
+	set(${DEPLOYED} FALSE PARENT_SCOPE)
+endif()
+endfunction(build_And_Install_Package)
+
 
