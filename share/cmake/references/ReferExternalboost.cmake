1dc5de59b142a04d36a7f683a80b0f2b7078876a
Robin Passama <passama@lirmm.fr>
2013-12-18 17:53:42 +0100

    changed first character of external packages' names from upper to lower case

---
+++ share/cmake/references/ReferExternalboost.cmake
@@ -0,0 +1,14 @@
+#### referencing external package boost ####
+set(boost_PID_Package_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(boost_PID_Package_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(boost_PID_Package_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
+set(boost_AUTHORS "Boost.org authors, see http://www.boost.org/" CACHE INTERNAL "")
+set(boost_LICENSES "Boost license" CACHE INTERNAL "")
+set(boost_DESCRIPTION external package providing many usefull C++ libraries, repackaged for PID CACHE INTERNAL "")
+set(boost_CATEGORIES system/threading;system/io;system/timing;language/container;language/meta CACHE INTERNAL "")
+set(boost_REFERENCES 1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0 linux CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux http://lirmm.lirmm.fr/FileX/get?auto=1&k=E5UWg8sZ08bN8HhVCY2 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_DEBUG http://lirmm.lirmm.fr/FileX/get?auto=1&k=RtFNxqdfYemEaGRqxdw CACHE INTERNAL "")
+
+
---
+++ share/cmake/references/ReferExternaleigen.cmake
@@ -0,0 +1,14 @@
+#### referencing package the-testpack-a mode ####
+set(eigen_PID_Package_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(eigen_PID_Package_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(eigen_PID_Package_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
+set(eigen_AUTHORS "Eigen developers team, see http://eigen.tuxfamily.org" CACHE INTERNAL "")
+set(eigen_LICENSES "MPL2" CACHE INTERNAL "")
+set(eigen_DESCRIPTION external package for linear algebra, repackaged for PID CACHE INTERNAL "")
+set(eigen_CATEGORIES math/linear_algebra;math/matrix CACHE INTERNAL "")
+set(eigen_REFERENCES 3.2.0 CACHE INTERNAL "")
+set(eigen_REFERENCE_3.2.0 linux CACHE INTERNAL "")
+set(eigen_REFERENCE_3.2.0_linux http://lirmm.lirmm.fr/FileX/get?auto=1&k=E5UWg8sZ08bN8HhVCY2 CACHE INTERNAL "")
+set(eigen_REFERENCE_3.2.0_linux_DEBUG http://lirmm.lirmm.fr/FileX/get?auto=1&k=RtFNxqdfYemEaGRqxdw CACHE INTERNAL "")
+
+
