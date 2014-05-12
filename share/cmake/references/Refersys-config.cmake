341bbaa43e59858aa810372062f24ec999b2cbbd
Robin Passama <passama@lirmm.fr>
2014-05-12 18:11:22 +0200

    adding sys-config reference file

---
+++ share/cmake/references/Refersys-config.cmake
@@ -0,0 +1,11 @@
+#### referencing package sys-config mode ####
+set(sys-config_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(sys-config_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(sys-config_CONTACT_MAIL  CACHE INTERNAL "")
+set(sys-config_AUTHORS_AND_INSTITUTIONS ;"_Robin_Passama(_LIRMM)" CACHE INTERNAL "")
+set(sys-config_DESCRIPTION set of libraries and tools used to manage configuration of programs and libraries CACHE INTERNAL "")
+set(sys-config_YEARS 2014 CACHE INTERNAL "")
+set(sys-config_LICENSE CeCILL-C CACHE INTERNAL "")
+set(sys-config_ADDRESS git@rob-dev.lirmm.fr:rob-developers/sys-config.git CACHE INTERNAL "")
+set(sys-config_CATEGORIES  CACHE INTERNAL "")
+set(sys-config_REFERENCES  CACHE INTERNAL "")
