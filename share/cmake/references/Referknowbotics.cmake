d966c5b8faef90b04317d0707a8c74567ef632b3
Robin Passama <passama@lirmm.fr>
2014-10-29 16:24:21 +0100

    changing references

---
+++ share/cmake/references/Referknowbotics.cmake
@@ -0,0 +1,11 @@
+#### referencing package knowbotics mode ####
+set(knowbotics_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(knowbotics_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(knowbotics_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
+set(knowbotics_AUTHORS_AND_INSTITUTIONS ;"_Robin_Passama(_LIRMM)" CACHE INTERNAL "")
+set(knowbotics_DESCRIPTION an API to ease integration of various robotics code CACHE INTERNAL "")
+set(knowbotics_YEARS 2014 CACHE INTERNAL "")
+set(knowbotics_LICENSE CeCILL-C CACHE INTERNAL "")
+set(knowbotics_ADDRESS git@rob-dev.lirmm.fr:rob/knowbotics.git CACHE INTERNAL "")
+set(knowbotics_CATEGORIES data/primitive;data/physic;data/geometry;middleware/integration CACHE INTERNAL "")
+set(knowbotics_REFERENCES  CACHE INTERNAL "")
