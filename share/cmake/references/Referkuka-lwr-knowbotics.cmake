92b1ac3b911a0328cf78982ec6167947f38af08b
Benjamin NAVARRO <benjamin.navarro@lirmm.fr>
2015-02-16 16:16:01 +0100

    add references

---
+++ share/cmake/references/Referkuka-lwr-knowbotics.cmake
@@ -0,0 +1,11 @@
+#### referencing package kuka-lwr-knowbotics mode ####
+set(kuka-lwr-knowbotics_MAIN_AUTHOR _Benjamin_Navarro CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_AUTHORS_AND_INSTITUTIONS ;"_Benjamin_Navarro(_LIRMM)" CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_DESCRIPTION A package that describes the Kuka LWR as a Knowbotics object CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_YEARS 20145 CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_LICENSE CeCILL-C CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_ADDRESS git@rob-dev.lirmm.fr:kuka-lwr-knowbotics/kuka-lwr-knowbotics.git CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_CATEGORIES robot/arm CACHE INTERNAL "")
+set(kuka-lwr-knowbotics_REFERENCES  CACHE INTERNAL "")
