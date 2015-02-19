7326e5916c2763c9c9d988935bf09e49fa799689
Robin Passama <passama@lirmm.fr>
2015-02-19 14:37:16 +0100

    updating references to boost and eigen

--- share/cmake/references/ReferExternalboost.cmake
+++ share/cmake/references/ReferExternalboost.cmake
@@ -6,8 +6,13 @@ set(boost_AUTHORS "Boost.org authors, see http://www.boost.org/" CACHE INTERNAL 
 set(boost_LICENSES "Boost license" CACHE INTERNAL "")
 set(boost_DESCRIPTION external package providing many usefull C++ libraries, repackaged for PID CACHE INTERNAL "")
 set(boost_CATEGORIES system/threading;system/io;system/timing;language/container;language/meta CACHE INTERNAL "")
+
 set(boost_REFERENCES 1.55.0 CACHE INTERNAL "")
-set(boost_REFERENCE_1.55.0 linux CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0 linux darwin CACHE INTERNAL "")
+
 set(boost_REFERENCE_1.55.0_linux_url https://rob-dev.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=linux-32b-1.55.0 CACHE INTERNAL "")
 set(boost_REFERENCE_1.55.0_linux_folder ext-boost.git CACHE INTERNAL "")
+
+set(boost_REFERENCE_1.55.0_darwin_url https://rob-dev.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=macosx-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_darwin_folder ext-boost.git CACHE INTERNAL "")
 
