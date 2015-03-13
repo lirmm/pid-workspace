2c30fd50ce55b0df1766aa7aaedaafc838e5cef9
Robin Passama <passama@lirmm.fr>
2015-03-13 20:09:48 +0100

    shadow-hand-udp-interface registered

---
+++ share/cmake/references/Refershadow-hand-udp-interface.cmake
@@ -0,0 +1,11 @@
+#### referencing package shadow-hand-udp-interface mode ####
+set(shadow-hand-udp-interface_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(shadow-hand-udp-interface_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(shadow-hand-udp-interface_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
+set(shadow-hand-udp-interface_AUTHORS_AND_INSTITUTIONS ; "_Robin_Passama(_LIRMM)" CACHE INTERNAL "")
+set(shadow-hand-udp-interface_DESCRIPTION UDP communication interface with the shadow hand PC supporting ROS driver CACHE INTERNAL "")
+set(shadow-hand-udp-interface_YEARS 2015 CACHE INTERNAL "")
+set(shadow-hand-udp-interface_LICENSE CeCILL-C CACHE INTERNAL "")
+set(shadow-hand-udp-interface_ADDRESS git@rob-dev.lirmm.fr:shadow-hands/shadow-hand-udp-interface.git CACHE INTERNAL "")
+set(shadow-hand-udp-interface_CATEGORIES robots/hand;drivers/hand CACHE INTERNAL "")
+set(shadow-hand-udp-interface_REFERENCES  CACHE INTERNAL "")
