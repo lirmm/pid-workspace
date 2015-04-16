e60b6cd041cc41fff9399f04aa4d9d8e1db3f45a
Benjamin NAVARRO <benjamin.navarro@lirmm.fr>
2015-04-16 09:03:39 +0200

    new reference files are old packets removed

---
+++ share/cmake/references/Referarm-controllers-knowbotics.cmake
@@ -0,0 +1,11 @@
+#### referencing package arm-controllers-knowbotics mode ####
+set(arm-controllers-knowbotics_MAIN_AUTHOR _Benjamin_Navarro CACHE INTERNAL "")
+set(arm-controllers-knowbotics_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(arm-controllers-knowbotics_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
+set(arm-controllers-knowbotics_AUTHORS_AND_INSTITUTIONS ; "_Benjamin_Navarro(_LIRMM)" CACHE INTERNAL "")
+set(arm-controllers-knowbotics_DESCRIPTION A set of different controllers for robotics arms CACHE INTERNAL "")
+set(arm-controllers-knowbotics_YEARS 2015 CACHE INTERNAL "")
+set(arm-controllers-knowbotics_LICENSE CeCILL-C CACHE INTERNAL "")
+set(arm-controllers-knowbotics_ADDRESS git@rob-dev.lirmm.fr:knowbotics/arm-controllers-knowbotics.git CACHE INTERNAL "")
+set(arm-controllers-knowbotics_CATEGORIES robot/arm CACHE INTERNAL "")
+set(arm-controllers-knowbotics_REFERENCES  CACHE INTERNAL "")
