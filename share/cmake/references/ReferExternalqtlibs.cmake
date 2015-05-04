2b81690fc74ccdd67f9011199d856d7b146b8d41
Benjamin NAVARRO <benjamin.navarro@lirmm.fr>
2015-05-04 10:33:25 +0200

    references to external package qtlibs

---
+++ share/cmake/references/ReferExternalqtlibs.cmake
@@ -0,0 +1,18 @@
+#### referencing external package qtlibs ####
+set(qtlibs_PID_Package_AUTHOR _Benjamin_Navarro CACHE INTERNAL "")
+set(qtlibs_PID_Package_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(qtlibs_PID_Package_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
+set(qtlibs_AUTHORS "qt.io authors, see http://www.qt.io/" CACHE INTERNAL "")
+set(qtlibs_LICENSES "qtlibs license" CACHE INTERNAL "")
+set(qtlibs_DESCRIPTION external package providing the Qt libraries libraries, repackaged for PID CACHE INTERNAL "")
+set(qtlibs_CATEGORIES system/threading;system/io;system/timing;language/container;language/meta CACHE INTERNAL "")
+
+set(qtlibs_REFERENCES 5.4.1 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1 linux darwin CACHE INTERNAL "")
+
+set(qtlibs_REFERENCE_5.4.1_linux_url https://rob-dev.lirmm.fr/pid_external_deps/ext-qtlibs/repository/archive.tar.gz?ref=linux-64b-5.4.1 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_linux_folder ext-qtlibs.git CACHE INTERNAL "")
+
+set(qtlibs_REFERENCE_5.4.1_darwin_url https://rob-dev.lirmm.fr/pid_external_deps/ext-qtlibs/repository/archive.tar.gz?ref=macosx-5.4.1 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_darwin_folder ext-qtlibs.git CACHE INTERNAL "")
+
