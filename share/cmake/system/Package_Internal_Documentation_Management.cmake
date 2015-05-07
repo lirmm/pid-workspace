4895b2dcabf28c2a58d40172f028df8c2844e7b7
Robin Passama <passama@lirmm.fr>
2015-05-07 20:34:54 +0200

    putting doxygen management in a new dedicated file

---
+++ share/cmake/system/Package_Internal_Documentation_Management.cmake
@@ -0,0 +1,134 @@
+
+
+### adding source code of the example components to the API doc
+function(add_Example_To_Doc c_name)
+	file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/share/examples/)
+	file(COPY ${${PROJECT_NAME}_${c_name}_TEMP_SOURCE_DIR} DESTINATION ${PROJECT_BINARY_DIR}/share/examples/)
+endfunction(add_Example_To_Doc c_name)
+
+### generating API documentation for the package
+function(generate_API)
+
+if(${CMAKE_BUILD_TYPE} MATCHES Release) # if in release mode we generate the doc
+
+if(NOT BUILD_API_DOC)
+	return()
+endif()
+
+#finding doxygen tool and doxygen configuration file 
+find_package(Doxygen)
+if(NOT DOXYGEN_FOUND)
+	message(WARNING "Doxygen not found please install it to generate the API documentation")
+	return()
+endif(NOT DOXYGEN_FOUND)
+find_file(DOXYFILE_IN   "Doxyfile.in"
+			PATHS "${CMAKE_SOURCE_DIR}/share/doxygen"
+			NO_DEFAULT_PATH
+	)
+set(DOXYFILE_IN ${DOXYFILE_IN} CACHE INTERNAL "")
+if(DOXYFILE_IN-NOTFOUND)
+	message(WARNING "Doxyfile not found in the share folder of your package !! Getting the standard doxygen template file from workspace ... ")
+	find_file(GENERIC_DOXYFILE_IN   "Doxyfile.in"
+					PATHS "${WORKSPACE_DIR}/share/cmake/patterns"
+					NO_DEFAULT_PATH
+		)
+	set(GENERIC_DOXYFILE_IN ${GENERIC_DOXYFILE_IN} CACHE INTERNAL "")
+	if(GENERIC_DOXYFILE_IN-NOTFOUND)
+		message(WARNING "No Template file found in ${WORKSPACE_DIR}/share/cmake/patterns/, skipping documentation generation !!")		
+	else(GENERIC_DOXYFILE_IN-NOTFOUND)
+		file(COPY ${WORKSPACE_DIR}/share/cmake/patterns/Doxyfile.in ${CMAKE_SOURCE_DIR}/share/doxygen)
+		message(STATUS "Template file found in ${WORKSPACE_DIR}/share/cmake/patterns/ and copied to your package, you can now modify it")		
+	endif(GENERIC_DOXYFILE_IN-NOTFOUND)
+endif(DOXYFILE_IN-NOTFOUND)
+
+if(DOXYGEN_FOUND AND (NOT DOXYFILE_IN-NOTFOUND OR NOT GENERIC_DOXYFILE_IN-NOTFOUND)) #we are able to generate the doc
+	# general variables
+	set(DOXYFILE_SOURCE_DIRS "${CMAKE_SOURCE_DIR}/include/")
+	set(DOXYFILE_PROJECT_NAME ${PROJECT_NAME})
+	set(DOXYFILE_PROJECT_VERSION ${${PROJECT_NAME}_VERSION})
+	set(DOXYFILE_OUTPUT_DIR ${CMAKE_BINARY_DIR}/share/doc)
+	set(DOXYFILE_HTML_DIR html)
+	set(DOXYFILE_LATEX_DIR latex)
+
+	### new targets ###
+	# creating the specific target to run doxygen
+	add_custom_target(doxygen
+		${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/share/Doxyfile
+		DEPENDS ${CMAKE_BINARY_DIR}/share/Doxyfile
+		WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+		COMMENT "Generating API documentation with Doxygen" VERBATIM
+	)
+
+	# target to clean installed doc
+	set_property(DIRECTORY
+		APPEND PROPERTY
+		ADDITIONAL_MAKE_CLEAN_FILES
+		"${DOXYFILE_OUTPUT_DIR}/${DOXYFILE_HTML_DIR}")
+
+	# creating the doc target
+	get_target_property(DOC_TARGET doc TYPE)
+	if(NOT DOC_TARGET)
+		add_custom_target(doc)
+	endif(NOT DOC_TARGET)
+
+	add_dependencies(doc doxygen)
+
+	### end new targets ###
+
+	### doxyfile configuration ###
+
+	# configuring doxyfile for html generation 
+	set(DOXYFILE_GENERATE_HTML "YES")
+
+	# configuring doxyfile to use dot executable if available
+	set(DOXYFILE_DOT "NO")
+	if(DOXYGEN_DOT_EXECUTABLE)
+		set(DOXYFILE_DOT "YES")
+	endif()
+
+	# configuring doxyfile for latex generation 
+	set(DOXYFILE_PDFLATEX "NO")
+
+	if(BUILD_LATEX_API_DOC)
+		# target to clean installed doc
+		set_property(DIRECTORY
+			APPEND PROPERTY
+			ADDITIONAL_MAKE_CLEAN_FILES
+			"${DOXYFILE_OUTPUT_DIR}/${DOXYFILE_LATEX_DIR}")
+		set(DOXYFILE_GENERATE_LATEX "YES")
+		find_package(LATEX)
+		find_program(DOXYFILE_MAKE make)
+		mark_as_advanced(DOXYFILE_MAKE)
+		if(LATEX_COMPILER AND MAKEINDEX_COMPILER AND DOXYFILE_MAKE)
+			if(PDFLATEX_COMPILER)
+				set(DOXYFILE_PDFLATEX "YES")
+			endif(PDFLATEX_COMPILER)
+
+			add_custom_command(TARGET doxygen
+				POST_BUILD
+				COMMAND "${DOXYFILE_MAKE}"
+				COMMENT	"Running LaTeX for Doxygen documentation in ${DOXYFILE_OUTPUT_DIR}/${DOXYFILE_LATEX_DIR}..."
+				WORKING_DIRECTORY "${DOXYFILE_OUTPUT_DIR}/${DOXYFILE_LATEX_DIR}")
+		else(LATEX_COMPILER AND MAKEINDEX_COMPILER AND DOXYFILE_MAKE)
+			set(DOXYGEN_LATEX "NO")
+		endif(LATEX_COMPILER AND MAKEINDEX_COMPILER AND DOXYFILE_MAKE)
+
+	else(BUILD_LATEX_API_DOC)
+		set(DOXYFILE_GENERATE_LATEX "NO")
+	endif(BUILD_LATEX_API_DOC)
+
+	#configuring the Doxyfile.in file to generate a doxygen configuration file
+	configure_file(${CMAKE_SOURCE_DIR}/share/doxygen/Doxyfile.in ${CMAKE_BINARY_DIR}/share/Doxyfile @ONLY)
+	### end doxyfile configuration ###
+
+	### installing documentation ###
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/share/doc DESTINATION ${${PROJECT_NAME}_INSTALL_SHARE_PATH})
+
+	### end installing documentation ###
+
+endif()
+	set(BUILD_API_DOC OFF FORCE)
+endif()
+endfunction(generate_API)
+
+
