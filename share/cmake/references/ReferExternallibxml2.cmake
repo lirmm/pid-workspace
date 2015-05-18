e4de40170748d3c0b1fc727fc5d010f0ebce55ae
Benjamin Navarro <navarro@lirmm.fr>
2015-05-18 11:57:20 +0200

    libxml2 reference files

---
+++ share/cmake/references/ReferExternallibxml2.cmake
@@ -0,0 +1,17 @@
+#### referencing external package libxml2 ####
+set(libxml2_PID_Package_AUTHOR _Benjamin_Navarro CACHE INTERNAL "")
+set(libxml2_PID_Package_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(libxml2_PID_Package_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
+set(libxml2_AUTHORS "qt.io authors, see http://www.qt.io/" CACHE INTERNAL "")
+set(libxml2_LICENSES "libxml2 license" CACHE INTERNAL "")
+set(libxml2_DESCRIPTION external package providing the LibXML2 libraries, repackaged for PID CACHE INTERNAL "")
+
+set(libxml2_REFERENCES 2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2 linux darwin CACHE INTERNAL "")
+
+set(libxml2_REFERENCE_2.9.2_linux_url https://rob-dev.lirmm.fr/pid_external_deps/ext-libxml2/repository/archive.tar.gz?ref=linux-32-64b-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux_folder ext-libxml2.git CACHE INTERNAL "")
+
+set(libxml2_REFERENCE_2.9.2_darwin_url https://rob-dev.lirmm.fr/pid_external_deps/ext-libxml2/repository/archive.tar.gz?ref=macosx-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_darwin_folder ext-libxml2.git CACHE INTERNAL "")
+
