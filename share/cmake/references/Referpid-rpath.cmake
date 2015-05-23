0f590e7abd16a64db59ed67ac0a5cb5a83fc1abe
Robin Passama <passama@lirmm.fr>
2015-05-23 14:04:30 +0200

    pid-rpath package registered

---
+++ share/cmake/references/Referpid-rpath.cmake
@@ -0,0 +1,11 @@
+#### referencing package pid-rpath mode ####
+set(pid-rpath_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(pid-rpath_MAIN_INSTITUTION _LIRMM_EMAIL_passama@lirmm.fr CACHE INTERNAL "")
+set(pid-rpath_CONTACT_MAIL  CACHE INTERNAL "")
+set(pid-rpath_AUTHORS_AND_INSTITUTIONS ; "_Robin_Passama(_LIRMM_EMAIL_passama@lirmm.fr)" CACHE INTERNAL "")
+set(pid-rpath_DESCRIPTION one of the default package of PID,used to manage runtime path of executables CACHE INTERNAL "")
+set(pid-rpath_YEARS 2015 CACHE INTERNAL "")
+set(pid-rpath_LICENSE CeCILL CACHE INTERNAL "")
+set(pid-rpath_ADDRESS git@rob-dev.lirmm.fr:pid/pid-rpath.git CACHE INTERNAL "")
+set(pid-rpath_CATEGORIES  CACHE INTERNAL "")
+set(pid-rpath_REFERENCES  CACHE INTERNAL "")
