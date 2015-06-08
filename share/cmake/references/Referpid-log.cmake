375384c4e44e3f990da000246231e63448b4e57b
Robin Passama <passama@lirmm.fr>
2015-06-08 16:31:44 +0200

    registering package pid-log

---
+++ share/cmake/references/Referpid-log.cmake
@@ -0,0 +1,11 @@
+#### referencing package pid-log mode ####
+set(pid-log_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(pid-log_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(pid-log_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
+set(pid-log_AUTHORS_AND_INSTITUTIONS ; "_Robin_Passama(_LIRMM)" CACHE INTERNAL "")
+set(pid-log_DESCRIPTION A package that defines libraries to easily manage logging CACHE INTERNAL "")
+set(pid-log_YEARS 2014 CACHE INTERNAL "")
+set(pid-log_LICENSE CeCILL-C CACHE INTERNAL "")
+set(pid-log_ADDRESS git@rob-dev.lirmm.fr:pid/pid-log.git CACHE INTERNAL "")
+set(pid-log_CATEGORIES  CACHE INTERNAL "")
+set(pid-log_REFERENCES  CACHE INTERNAL "")
