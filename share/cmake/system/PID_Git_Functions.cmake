8b5c6c984cd40b2e456b150443b7094437852c0d
Robin Passama <passama@lirmm.fr>
2015-07-02 12:43:50 +0200

    always create an integration branch when cloning

--- share/cmake/system/PID_Git_Functions.cmake
+++ share/cmake/system/PID_Git_Functions.cmake
@@ -260,6 +260,7 @@ if(EXISTS ${WORKSPACE_DIR}/packages/${package} AND IS_DIRECTORY ${WORKSPACE_DIR}
 	set(${IS_DEPLOYED} TRUE PARENT_SCOPE)
 	execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${WORKSPACE_DIR}/packages/${package} git fetch origin OUTPUT_QUIET ERROR_QUIET)
 	execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${WORKSPACE_DIR}/packages/${package} git fetch origin --tags OUTPUT_QUIET ERROR_QUIET)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${WORKSPACE_DIR}/packages/${package} git checkout integration OUTPUT_QUIET ERROR_QUIET)
 else()
 	set(${IS_DEPLOYED} FALSE PARENT_SCOPE)
 	message("[ERROR] : impossible to clone the repository of package ${package} (bad repository address or you have no clone rights for this repository). Please contact the administrator of this package.")
