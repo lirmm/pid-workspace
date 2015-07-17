aed2daa1a92bbc43e2923a2611c453a44590ec64
Robin Passama <passama@lirmm.fr>
2015-07-17 12:18:56 +0200

    adding PID cmake scripts to manage DLINK camera

---
+++ share/cmake/references/Referptz-dlink-dcs5222l.cmake
@@ -0,0 +1,11 @@
+#### referencing package ptz-dlink-dcs5222l mode ####
+set(ptz-dlink-dcs5222l_MAIN_AUTHOR _Robin_Passama CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_CONTACT_MAIL  CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_AUTHORS_AND_INSTITUTIONS ; "_Robin_Passama(_LIRMM)"; "_Florian_Raffalli(_LIRMM)" CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_DESCRIPTION Driver;software;to;deal;with;the;DCS522L;DLINK;camera CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_YEARS 2015 CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_LICENSE CeCILL-C CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_ADDRESS git@rob-dev.lirmm.fr:cameras/ptz-dlink-dcs5222l.git CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_CATEGORIES  CACHE INTERNAL "")
+set(ptz-dlink-dcs5222l_REFERENCES  CACHE INTERNAL "")
