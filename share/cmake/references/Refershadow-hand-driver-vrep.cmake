08d3106bcb2d742e9c16a3fe99056abb84bc3dd2
Robin Passama <passama@lirmm.fr>
2015-10-06 14:11:48 +0200

    new reference for shadow vrep

--- share/cmake/references/Refershadow-hand-driver-vrep.cmake
+++ share/cmake/references/Refershadow-hand-driver-vrep.cmake
@@ -2,7 +2,7 @@
 set(shadow-hand-driver-vrep_MAIN_AUTHOR _Benjamin_Navarro CACHE INTERNAL "")
 set(shadow-hand-driver-vrep_MAIN_INSTITUTION _LIRMM CACHE INTERNAL "")
 set(shadow-hand-driver-vrep_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
-set(shadow-hand-driver-vrep_AUTHORS_AND_INSTITUTIONS ; "_Benjamin_Navarro(_LIRMM)" CACHE INTERNAL "")
+set(shadow-hand-driver-vrep_AUTHORS_AND_INSTITUTIONS "_Benjamin_Navarro(_LIRMM)";"_Robin_Passama(_LIRMM)" CACHE INTERNAL "")
 set(shadow-hand-driver-vrep_DESCRIPTION A package to interface with a simulated Shadow Dexterous hand on V-REP CACHE INTERNAL "")
 set(shadow-hand-driver-vrep_YEARS 2015 CACHE INTERNAL "")
 set(shadow-hand-driver-vrep_LICENSE CeCILL-C CACHE INTERNAL "")
