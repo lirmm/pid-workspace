2ec424241fc9c60a21c583ba19b6957d5444cf94
Robin Passama <passama@lirmm.fr>
2015-11-23 18:08:42 +0100

    updated ref to boost

--- share/cmake/references/ReferExternalboost.cmake
+++ share/cmake/references/ReferExternalboost.cmake
@@ -5,14 +5,14 @@ set(boost_PID_Package_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
 set(boost_AUTHORS "Boost.org authors, see http://www.boost.org/" CACHE INTERNAL "")
 set(boost_LICENSES "Boost license" CACHE INTERNAL "")
 set(boost_DESCRIPTION external package providing many usefull C++ libraries, repackaged for PID CACHE INTERNAL "")
-set(boost_CATEGORIES system/threading;system/io;system/timing;language/container;language/meta CACHE INTERNAL "")
+set(boost_CATEGORIES programming/threading;programming/io;programming/timing;programming/container;programming/meta CACHE INTERNAL "")
 
 set(boost_REFERENCES 1.55.0 CACHE INTERNAL "")
 set(boost_REFERENCE_1.55.0 linux darwin CACHE INTERNAL "")
 
-set(boost_REFERENCE_1.55.0_linux_url https://rob-dev.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=linux-32-64b-1.55.0 CACHE INTERNAL "")
-set(boost_REFERENCE_1.55.0_linux_folder ext-boost.git CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_url https://gite.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=linux-32-64b-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_folder ext-boost-linux-32-64b-1.55.0-fc88e6d168ad7e9ad25d312526af5454b95961f0 CACHE INTERNAL "")
 
-set(boost_REFERENCE_1.55.0_darwin_url https://rob-dev.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=macosx-1.55.0 CACHE INTERNAL "")
-set(boost_REFERENCE_1.55.0_darwin_folder ext-boost.git CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_darwin_url https://gite.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=macosx-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_darwin_folder ext-boost-macosx-1.55.0-3e324f1ac45c77dc5e64a9ac705261f62ac1f0ba CACHE INTERNAL "")
 
