816f062c1b882dff66ff25770d6275abbe77dadb
Robin Passama <passama@lirmm.fr>
2015-11-23 18:01:26 +0100

    updated ref to xml2

--- share/cmake/references/ReferExternallibxml2.cmake
+++ share/cmake/references/ReferExternallibxml2.cmake
@@ -5,6 +5,7 @@ set(libxml2_PID_Package_CONTACT_MAIL navarro@lirmm.fr CACHE INTERNAL "")
 set(libxml2_AUTHORS "qt.io authors, see http://www.qt.io/" CACHE INTERNAL "")
 set(libxml2_LICENSES "libxml2 license" CACHE INTERNAL "")
 set(libxml2_DESCRIPTION external package providing the LibXML2 libraries, repackaged for PID CACHE INTERNAL "")
+set(libxml2_CATEGORIES programming/parser CACHE INTERNAL "")
 
 set(libxml2_REFERENCES 2.9.2 CACHE INTERNAL "")
 set(libxml2_REFERENCE_2.9.2 linux darwin CACHE INTERNAL "")
