bdfa2ba27ba7127451ddf2051ffeed9f9074ee7f
Robin Passama <passama@lirmm.fr>
2016-02-09 10:49:13 +0100

    updated yaml-cpp reference (now in PID group with separate linux 32 & 64 bits arch)

--- share/cmake/references/ReferExternalyaml-cpp.cmake
+++ share/cmake/references/ReferExternalyaml-cpp.cmake
@@ -13,15 +13,15 @@ set(yaml-cpp_REFERENCE_0.5.1_linux 32 64 CACHE INTERNAL "")
 set(yaml-cpp_REFERENCE_0.5.1_macosx 64 CACHE INTERNAL "")
 
 #linux 32
-set(yaml-cpp_REFERENCE_0.5.1_linux_32_URL https://gite.lirmm.fr/pid_external_deps/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-32-64b-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_linux_32_FOLDER ext-yaml-cpp-linux-32-64b-0.5.1-38b17185c58c269f702b6561e7c54773fe9d7447 CACHE INTERNAL "")
+set(yaml-cpp_REFERENCE_0.5.1_linux_32_URL https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-32-0.5.1 CACHE INTERNAL "")
+set(yaml-cpp_REFERENCE_0.5.1_linux_32_FOLDER ext-yaml-cpp-linux-32-0.5.1-75a6c1b5e6b9979875883cd0caed58a7442f7dee CACHE INTERNAL "")
 
 #linux 64
-set(yaml-cpp_REFERENCE_0.5.1_linux_64_URL https://gite.lirmm.fr/pid_external_deps/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-32-64b-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_linux_64_FOLDER ext-yaml-cpp-linux-32-64b-0.5.1-38b17185c58c269f702b6561e7c54773fe9d7447 CACHE INTERNAL "")
+set(yaml-cpp_REFERENCE_0.5.1_linux_64_URL https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-64-0.5.1 CACHE INTERNAL "")
+set(yaml-cpp_REFERENCE_0.5.1_linux_64_FOLDER ext-yaml-cpp-linux-64-0.5.1-50fb447084ba61dc0db8b21b4aa53a5978d6139e CACHE INTERNAL "")
 
 #macosx 64
-set(yaml-cpp_REFERENCE_0.5.1_macosx_64_url https://gite.lirmm.fr/pid_external_deps/ext-yaml-cpp/repository/archive.tar.gz?ref=macosx-0.5.1 CACHE INTERNAL "")
+set(yaml-cpp_REFERENCE_0.5.1_macosx_64_url https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=macosx-0.5.1 CACHE INTERNAL "")
 set(yaml-cpp_REFERENCE_0.5.1_macosx_64_FOLDER ext-yaml-cpp-macosx-0.5.1-83d48489ea31426ab801fc0349310e63b1c6e3bf CACHE INTERNAL "")
 
 
