783ee95145472671bcb943fc5dbd78ca8dfa6be9
Robin Passama <passama@lirmm.fr>
2016-02-09 10:33:08 +0100

    updated ref to boost (now directly in pid project)

--- share/cmake/references/ReferExternalboost.cmake
+++ share/cmake/references/ReferExternalboost.cmake
@@ -13,14 +13,14 @@ set(boost_REFERENCE_1.55.0_linux 32 64 CACHE INTERNAL "")
 set(boost_REFERENCE_1.55.0_macosx 64 CACHE INTERNAL "")
 
 #linux 32
-set(boost_REFERENCE_1.55.0_linux_32_URL https://gite.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=linux-32-64b-1.55.0 CACHE INTERNAL "")
-set(boost_REFERENCE_1.55.0_linux_32_FOLDER ext-boost-linux-32-64b-1.55.0-fc88e6d168ad7e9ad25d312526af5454b95961f0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_32_URL https://gite.lirmm.fr/pid/ext-boost/repository/archive.tar.gz?ref=linux-32-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_32_FOLDER ext-boost-linux-32-1.55.0-be4c416cad5168d56419d4a3e20a034bb8a08a46 CACHE INTERNAL "")
 
-#linux 32
-set(boost_REFERENCE_1.55.0_linux_64_URL https://gite.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=linux-32-64b-1.55.0 CACHE INTERNAL "")
-set(boost_REFERENCE_1.55.0_linux_64_FOLDER ext-boost-linux-32-64b-1.55.0-fc88e6d168ad7e9ad25d312526af5454b95961f0 CACHE INTERNAL "")
+#linux 64
+set(boost_REFERENCE_1.55.0_linux_64_URL https://gite.lirmm.fr/pid/ext-boost/repository/archive.tar.gz?ref=linux-64-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_linux_64_FOLDER ext-boost-linux-64-1.55.0-1d1f1f82091112c799d135aefdec2e5883e0538d CACHE INTERNAL "")
 
 #macosx 64
-set(boost_REFERENCE_1.55.0_macosx_64_URL https://gite.lirmm.fr/pid_external_deps/ext-boost/repository/archive.tar.gz?ref=macosx-1.55.0 CACHE INTERNAL "")
+set(boost_REFERENCE_1.55.0_macosx_64_URL https://gite.lirmm.fr/pid/ext-boost/repository/archive.tar.gz?ref=macosx-1.55.0 CACHE INTERNAL "")
 set(boost_REFERENCE_1.55.0_macosx_64_FOLDER ext-boost-macosx-1.55.0-3e324f1ac45c77dc5e64a9ac705261f62ac1f0ba CACHE INTERNAL "")
 
