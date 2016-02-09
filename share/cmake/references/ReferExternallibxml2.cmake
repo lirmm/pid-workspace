16a7870125f887f929627a508d1b5a3a18eddfca
Robin Passama <passama@lirmm.fr>
2016-02-09 15:55:15 +0100

    changed ref to libxml2

--- share/cmake/references/ReferExternallibxml2.cmake
+++ share/cmake/references/ReferExternallibxml2.cmake
@@ -14,12 +14,12 @@ set(libxml2_REFERENCE_2.9.2_linux 32 64)
 set(libxml2_REFERENCE_2.9.2_macosx 64)
 
 #linux32
-set(libxml2_REFERENCE_2.9.2_linux_32_URL https://gite.lirmm.fr/pid_external_deps/ext-libxml2/repository/archive.tar.gz?ref=linux-32-64b-2.9.2 CACHE INTERNAL "")
-set(libxml2_REFERENCE_2.9.2_linux_32_FOLDER ext-libxml2-linux-32-64b-2.9.2-008a4a6cad0f1f0a16961dae3a4a34fb06b6a035 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux_32_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=linux-32-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux_32_FOLDER ext-libxml2-linux-32-2.9.2-1b9a9d4fa5cb3b8f69dd057b4b87c1866cbdef63 CACHE INTERNAL "")
 #linux64
-set(libxml2_REFERENCE_2.9.2_linux_64_URL https://gite.lirmm.fr/pid_external_deps/ext-libxml2/repository/archive.tar.gz?ref=linux-32-64b-2.9.2 CACHE INTERNAL "")
-set(libxml2_REFERENCE_2.9.2_linux_64_FOLDER ext-libxml2-linux-32-64b-2.9.2-008a4a6cad0f1f0a16961dae3a4a34fb06b6a035 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux_64_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=linux-64-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux_64_FOLDER ext-libxml2-linux-64-2.9.2-8f7d4765e022e19c98e27f29ed6fe4eed29b0e07 CACHE INTERNAL "")
 #macosx64
-set(libxml2_REFERENCE_2.9.2_macosx_64_URL https://gite.lirmm.fr/pid_external_deps/ext-libxml2/repository/archive.tar.gz?ref=macosx-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_macosx_64_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=macosx-2.9.2 CACHE INTERNAL "")
 set(libxml2_REFERENCE_2.9.2_macosx_64_FOLDER ext-libxml2-macosx-2.9.2-e010d91f0c7969af5f5bb541ad14719cf3eee966 CACHE INTERNAL "")
 
