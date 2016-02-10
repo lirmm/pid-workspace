6667e633864afaf0e2c54aef4153b075c6813391
Robin Passama <passama@lirmm.fr>
2016-02-10 12:34:05 +0100

    new references for ffmpeg

--- share/cmake/references/ReferExternalffmpeg.cmake
+++ share/cmake/references/ReferExternalffmpeg.cmake
@@ -15,10 +15,10 @@ set(ffmpeg_REFERENCE_2.7.1_linux 32 64 CACHE INTERNAL "")
 
 #linux 32
 set(ffmpeg_REFERENCE_2.7.1_linux_32_URL https://gite.lirmm.fr/pid/ext-ffmpeg/repository/archive.tar.gz?ref=linux-32-2.7.1 CACHE INTERNAL "")
-set(ffmpeg_REFERENCE_2.7.1_linux_32_FOLDER ext-ffmpeg-linux-32-2.7.1-3d9972ed063b850b6f6048431a76f3b97a2eb7ce CACHE INTERNAL "")
+set(ffmpeg_REFERENCE_2.7.1_linux_32_FOLDER ext-ffmpeg-linux-32-2.7.1-b005d0ae1584a59850830ffb683e3bdcb274625e CACHE INTERNAL "")
 #linux 64
 set(ffmpeg_REFERENCE_2.7.1_linux_64_URL https://gite.lirmm.fr/pid/ext-ffmpeg/repository/archive.tar.gz?ref=linux-64-2.7.1 CACHE INTERNAL "")
-set(ffmpeg_REFERENCE_2.7.1_linux_64_FOLDER ext-ffmpeg-linux-64-2.7.1-944c48db063ee693bf468800c432248d97992be5 CACHE INTERNAL "")
+set(ffmpeg_REFERENCE_2.7.1_linux_64_FOLDER ext-ffmpeg-linux-64-2.7.1-73e535c5c664ba25ee51f6c48b36df20cd06fd50 CACHE INTERNAL "")
 
 ######## #version 2.8.2 ###########
 set(ffmpeg_REFERENCE_2.8.2 linux CACHE INTERNAL "")
@@ -26,6 +26,6 @@ set(ffmpeg_REFERENCE_2.8.2_linux 64 CACHE INTERNAL "")
 
 #linux 64
 set(ffmpeg_REFERENCE_2.8.2_linux_64_URL https://gite.lirmm.fr/pid/ext-ffmpeg/repository/archive.tar.gz?ref=linux-64-2.8.2 CACHE INTERNAL "")
-set(ffmpeg_REFERENCE_2.8.2_linux_64_FOLDER ext-ffmpeg-linux-64-2.8.2-d8d50b80bf41663d55a6c8aaaf06a7fd466f9c84 CACHE INTERNAL "")
+set(ffmpeg_REFERENCE_2.8.2_linux_64_FOLDER ext-ffmpeg-linux-64-2.8.2-f7512067191661e9c90e4ef6ac820676074d5def CACHE INTERNAL "")
 
 
