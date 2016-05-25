1502410b7d035427c346e7a8f3a3edb3ff4763f7
Robin Passama <passama@lirmm.fr>
2016-05-25 12:10:46 +0200

    updated reference for opencv

--- share/cmake/references/ReferExternalopencv.cmake
+++ share/cmake/references/ReferExternalopencv.cmake
@@ -9,19 +9,19 @@ set(opencv_CATEGORIES image vision CACHE INTERNAL "")
 
 
 #declaration of possible platforms
-set(opencv_AVAILABLE_PLATFORMS linux64;linux32;macosx64;macosx32 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORMS linux64cxx11;linux64;linux32 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux64cxx11_OS linux CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux64cxx11_ARCH 64 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux64cxx11_ABI CXX11 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux64cxx11_CONFIGURATION  CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux64_OS linux CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux64_ARCH 64 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux64_ABI CXX CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux64_CONFIGURATION  CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux32_OS linux CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux32_ARCH 32 CACHE INTERNAL "")
+set(opencv_AVAILABLE_PLATFORM_linux32_ABI CXX CACHE INTERNAL "")
 set(opencv_AVAILABLE_PLATFORM_linux32_CONFIGURATION  CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx64_OS macosx CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx64_ARCH 64 CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx64_CONFIGURATION  CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx32_OS macosx CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx32_ARCH 32 CACHE INTERNAL "")
-set(opencv_AVAILABLE_PLATFORM_macosx32_CONFIGURATION  CACHE INTERNAL "")
 
 # declaration of known references
 set(opencv_REFERENCES 2.4.11 CACHE INTERNAL "")
@@ -34,3 +34,8 @@ set(opencv_REFERENCE_2.4.11_linux32_FOLDER ext-opencv-linux-32-2.4.11-691f1f1dfe
 #linux 64 
 set(opencv_REFERENCE_2.4.11_linux64_URL https://gite.lirmm.fr/pid/ext-opencv/repository/archive.tar.gz?ref=linux-64-2.4.11 CACHE INTERNAL "")
 set(opencv_REFERENCE_2.4.11_linux64_FOLDER ext-opencv-linux-64-2.4.11-3438bee806663c2d17517c40f26d17d506b61298 CACHE INTERNAL "")
+
+#linux64cxx11
+
+set(opencv_REFERENCE_2.4.11_linux64cxx11_URL https://gite.lirmm.fr/pid/ext-opencv/repository/archive.tar.gz?ref=linux-64-cxx11-2.4.11 CACHE INTERNAL "")
+set(opencv_REFERENCE_2.4.11_linux64cxx11_FOLDER ext-opencv-linux-64-cxx11-2.4.11-87d4c6d4d43c230daefc6d6acbed9162d17f9240 CACHE INTERNAL "")
