a17952a7c68e3282af972dce0afb92ac424ab631
Robin Passama <passama@lirmm.fr>
2016-06-06 11:47:40 +0200

    updating references

--- share/cmake/references/ReferExternallibxml2.cmake
+++ share/cmake/references/ReferExternallibxml2.cmake
@@ -9,31 +9,41 @@ set(libxml2_CATEGORIES programming/parser CACHE INTERNAL "")
 
 
 #declaration of possible platforms
-set(libxml2_AVAILABLE_PLATFORMS linux64;linux32;macosx64;macosx32 CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORMS linux64cxx11;linux64;linux32;macosx64 CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux64cxx11_OS linux CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux64cxx11_ARCH 64 CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux64cxx11_ABI CXX11 CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_llinux64cxx11_CONFIGURATION posix CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_linux64_OS linux CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_linux64_ARCH 64 CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_linux64_CONFIGURATION  CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux64_ABI CXX CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux64_CONFIGURATION posix CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_linux32_OS linux CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_linux32_ARCH 32 CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_linux32_CONFIGURATION  CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux32_ABI CXX CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_linux32_CONFIGURATION posix CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_macosx64_OS macosx CACHE INTERNAL "")
 set(libxml2_AVAILABLE_PLATFORM_macosx64_ARCH 64 CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_macosx64_CONFIGURATION  CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_macosx32_OS macosx CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_macosx32_ARCH 32 CACHE INTERNAL "")
-set(libxml2_AVAILABLE_PLATFORM_macosx32_CONFIGURATION  CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_macosx64_CONFIGURATION posix CACHE INTERNAL "")
+set(libxml2_AVAILABLE_PLATFORM_macosx64_ABI CXX CACHE INTERNAL "")
 
 # declaration of known references
 
 set(libxml2_REFERENCES 2.9.2 CACHE INTERNAL "")
-set(libxml2_REFERENCE_2.9.2 linux32 linux64 macosx64 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2 linux64cxx11 linux64 linux32 macosx64 CACHE INTERNAL "")
 
 #linux32
 set(libxml2_REFERENCE_2.9.2_linux32_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=linux-32-2.9.2 CACHE INTERNAL "")
 set(libxml2_REFERENCE_2.9.2_linux32_FOLDER ext-libxml2-linux-32-2.9.2-4d46d21ae6b90e4084d2bbe30059e310a5e44168 CACHE INTERNAL "")
 #linux64
 set(libxml2_REFERENCE_2.9.2_linux64_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=linux-64-2.9.2 CACHE INTERNAL "")
-set(libxml2_REFERENCE_2.9.2_linux64_FOLDER ext-libxml2-linux-64-2.9.2-5a1ef8db8ef180e7a90ac1806dc1f6736c8a89d5 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux64_FOLDER ext-libxml2-linux-64-2.9.2-40e52ffbbf3680826d6b0ec013546ab47fadcf4a CACHE INTERNAL "")
+
+#linux64cxx11
+set(libxml2_REFERENCE_2.9.2_linux64cxx11_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=linux-64cxx11-2.9.2 CACHE INTERNAL "")
+set(libxml2_REFERENCE_2.9.2_linux64cxx11_FOLDER ext-libxml2-linux-64cxx11-2.9.2-301748bd2e2f96eb2211a181dc0314fbf34e0cf7 CACHE INTERNAL "")
+
+
 #macosx64
 set(libxml2_REFERENCE_2.9.2_macosx64_URL https://gite.lirmm.fr/pid/ext-libxml2/repository/archive.tar.gz?ref=macosx-2.9.2 CACHE INTERNAL "")
 set(libxml2_REFERENCE_2.9.2_macosx64_FOLDER ext-libxml2-macosx-2.9.2-29aa51df94450601a9bfc8616d029918b385639f CACHE INTERNAL "")
