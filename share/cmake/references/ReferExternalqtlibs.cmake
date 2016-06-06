a597f40ae7809f4fe3846f95d67507c2aa440402
Robin Passama <passama@lirmm.fr>
2016-06-06 13:05:59 +0200

    updating opengl configuration (using freeglut3-dev) + chaging ref to qtlibs

--- share/cmake/references/ReferExternalqtlibs.cmake
+++ share/cmake/references/ReferExternalqtlibs.cmake
@@ -9,7 +9,11 @@ set(qtlibs_CATEGORIES programming/threading programming/io programming/timing pr
 
 
 #declaration of possible platforms
-set(qtlibs_AVAILABLE_PLATFORMS linux64;linux32;macosx64;macosx32 CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORMS linux64cxx11;linux64;linux32;macosx64 CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORM_linux64cxx11_OS linux64cxx11 CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORM_linux64cxx11_ARCH 64 CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORM_linux64cxx11_ABI CXX11 CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORM_linux64cxx11_CONFIGURATION x11 opengl CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_linux64_OS linux CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_linux64_ARCH 64 CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_linux64_CONFIGURATION x11 opengl CACHE INTERNAL "")
@@ -18,22 +22,27 @@ set(qtlibs_AVAILABLE_PLATFORM_linux32_ARCH 32 CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_linux32_CONFIGURATION x11 opengl CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_macosx64_OS macosx CACHE INTERNAL "")
 set(qtlibs_AVAILABLE_PLATFORM_macosx64_ARCH 64 CACHE INTERNAL "")
-set(qtlibs_AVAILABLE_PLATFORM_macosx64_CONFIGURATION opengl CACHE INTERNAL "")
+set(qtlibs_AVAILABLE_PLATFORM_macosx64_CONFIGURATION CACHE INTERNAL "")
 
 # declaration of known references
 set(qtlibs_REFERENCES 5.4.1 CACHE INTERNAL "")
-set(qtlibs_REFERENCE_5.4.1 linux32 linux64 macosx64 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1 linux64cxx11 linux32 linux64 macosx64 CACHE INTERNAL "")
 
 #linux 32
 set(qtlibs_REFERENCE_5.4.1_linux32_URL https://gite.lirmm.fr/pid/ext-qtlibs/repository/archive.tar.gz?ref=linux-32-5.4.1 CACHE INTERNAL "")
-set(qtlibs_REFERENCE_5.4.1_linux32_FOLDER ext-qtlibs-linux-32-5.4.1-f357e44170031f23c91f0897d21c01c4cad4fadf CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_linux32_FOLDER ext-qtlibs-linux-32-5.4.1-d3b1e8730c48f326ee54b35c4b3555a2046b4f79 CACHE INTERNAL "")
+
+
+#linux 64 cxx11
+set(qtlibs_REFERENCE_5.4.1_linux64cxx11_URL https://gite.lirmm.fr/pid/ext-qtlibs/repository/archive.tar.gz?ref=linux-64cxx11-5.4.1 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_linux64cxx11_FOLDER ext-qtlibs-linux-64cxx11-5.4.1-4dfe67a0d19f66a15c1ae7cfbca620f1167f6394 CACHE INTERNAL "")
 
 #linux 64
 set(qtlibs_REFERENCE_5.4.1_linux64_URL https://gite.lirmm.fr/pid/ext-qtlibs/repository/archive.tar.gz?ref=linux-64-5.4.1 CACHE INTERNAL "")
-set(qtlibs_REFERENCE_5.4.1_linux64_FOLDER ext-qtlibs-linux-64-5.4.1-721dd518da12100632e4c0c2cdd1fbd636107cb0 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_linux64_FOLDER ext-qtlibs-linux-64-5.4.1-e51168017730a6b17f4dd036e9f898eeef1014ed CACHE INTERNAL "")
 
 #macosx 64
-set(qtlibs_REFERENCE_5.4.1_macosx64_URL https://gite.lirmm.fr/pid/ext-qtlibs/repository/archive.tar.gz?ref=macosx-5.4.1 CACHE INTERNAL "")
-set(qtlibs_REFERENCE_5.4.1_macosx64_FOLDER ext-qtlibs-macosx-5.4.1-ef3b4fb81f28c51e02a6668f5ecc79f5b2585000 CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_macosx64_URL https://gite.lirmm.fr/pid/ext-qtlibs/repository/archive.tar.gz?ref=macosx-5.4.1CACHE INTERNAL "")
+set(qtlibs_REFERENCE_5.4.1_macosx64_FOLDER ext-qtlibs-macosx-5.4.1-cd27fc4c101f1e425982495cc2e0bc205a15dee8 CACHE INTERNAL "")
 
 
