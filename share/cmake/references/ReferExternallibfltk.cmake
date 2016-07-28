ef2cbfa6d38a543f981176357ebe85731d85a744
Robin Passama <passama@lirmm.fr>
2016-07-28 10:07:52 +0200

    new references for libfltk and freetype2

--- share/cmake/references/ReferExternallibfltk.cmake
+++ share/cmake/references/ReferExternallibfltk.cmake
@@ -9,25 +9,28 @@ set(libfltk_CATEGORIES programming/gui CACHE INTERNAL "")
 
 
 #declaration of possible platforms
-set(libfltk_AVAILABLE_PLATFORMS linux64;linux32;macosx64;macosx32 CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORMS linux64cxx11;linux64;linux32 CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux64cxx11_OS linux CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux64cxx11_ARCH 64 CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux64cxx11_ABI CXX11 CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux64cxx11_CONFIGURATION posix x11 CACHE INTERNAL "")
 set(libfltk_AVAILABLE_PLATFORM_linux64_OS linux CACHE INTERNAL "")
 set(libfltk_AVAILABLE_PLATFORM_linux64_ARCH 64 CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_linux64_CONFIGURATION  CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux64_CONFIGURATION posix x11 CACHE INTERNAL "")
 set(libfltk_AVAILABLE_PLATFORM_linux32_OS linux CACHE INTERNAL "")
 set(libfltk_AVAILABLE_PLATFORM_linux32_ARCH 32 CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_linux32_CONFIGURATION  CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx64_OS macosx CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx64_ARCH 64 CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx64_CONFIGURATION  CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx32_OS macosx CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx32_ARCH 32 CACHE INTERNAL "")
-set(libfltk_AVAILABLE_PLATFORM_macosx32_CONFIGURATION  CACHE INTERNAL "")
+set(libfltk_AVAILABLE_PLATFORM_linux32_CONFIGURATION posix x11 CACHE INTERNAL "")
 
 # declaration of known references
 set(libfltk_REFERENCES 1.3.3 CACHE INTERNAL "")
-set(libfltk_REFERENCE_1.3.3 linux64 CACHE INTERNAL "")
+set(libfltk_REFERENCE_1.3.3 linux64 linux64cxx11 CACHE INTERNAL "")
 
 #linux 64
 set(libfltk_REFERENCE_1.3.3_linux64_URL https://gite.lirmm.fr/pid/ext-libfltk/repository/archive.tar.gz?ref=linux-64-1.3.3 CACHE INTERNAL "")
 set(libfltk_REFERENCE_1.3.3_linux64_FOLDER ext-libfltk-linux-64-1.3.3-7a7d9c369a2d6c6744aad6b68c6280d80a0a6874 CACHE INTERNAL "")
+
+
+#linux 64 cxx11 ABI
+set(libfltk_REFERENCE_1.3.3_linux64cxx11_URL https://gite.lirmm.fr/pid/ext-libfltk/repository/archive.tar.gz?ref=linux-64-cxx11-1.3.3 CACHE INTERNAL "")
+set(libfltk_REFERENCE_1.3.3_linux64cxx11_FOLDER ext-libfltk-linux-64-cxx11-1.3.3-7caf18efc80516bb9866be3917827cf62c17d003 CACHE INTERNAL "")
 
