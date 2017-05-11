6eb7a14b95fa7d326d48c7393d2333eb255fdd3f
Robin Passama <passama@lirmm.fr>
2017-05-11 19:30:56 +0200

    updated reference for pid framework

--- share/cmake/references/ReferExternalboost.cmake
+++ share/cmake/references/ReferExternalboost.cmake
@@ -5,7 +5,7 @@ set(boost_PID_Package_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
 set(boost_AUTHORS "Boost.org authors, see http://www.boost.org/" CACHE INTERNAL "")
 set(boost_LICENSES "Boost license" CACHE INTERNAL "")
 set(boost_DESCRIPTION external package providing many usefull C++ libraries, repackaged for PID CACHE INTERNAL "")
-set(eigen_FRAMEWORK pid CACHE INTERNAL "")
+set(boost_FRAMEWORK pid CACHE INTERNAL "")
 set(boost_CATEGORIES programming/threading;programming/io;programming/timing;programming/container;programming/meta CACHE INTERNAL "")
 
 
