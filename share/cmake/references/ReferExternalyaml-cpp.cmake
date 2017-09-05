021627bafe9184428cdecc981716b4ccbc8b4394
Robin Passama <robin.passama@lirmm.fr>
2017-09-05 15:56:40 +0200

    updated references

--- share/cmake/references/ReferExternalyaml-cpp.cmake
+++ share/cmake/references/ReferExternalyaml-cpp.cmake
@@ -8,27 +8,3 @@ set(yaml-cpp_DESCRIPTION a YAML parser library for C++ CACHE INTERNAL "")
 set(yaml-cpp_CATEGORIES programming/configuration CACHE INTERNAL "")
 set(yaml-cpp_FRAMEWORK pid CACHE INTERNAL "")
 
-# declaration of known references
-
-set(yaml-cpp_REFERENCES 0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1 x86_32_linux_abi98 x86_64_linux_abi98 x86_64_macosx_abi98 x86_64_linux_abi11 CACHE INTERNAL "")
-
-#linux 32
-set(yaml-cpp_REFERENCE_0.5.1_x86_32_linux_abi98_URL https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-32-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_x86_32_linux_abi98_FOLDER ext-yaml-cpp-linux-32-0.5.1-75a6c1b5e6b9979875883cd0caed58a7442f7dee CACHE INTERNAL "")
-
-#linux 64
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_linux_abi98_URL https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-64-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_linux_abi98_FOLDER ext-yaml-cpp-linux-64-0.5.1-50fb447084ba61dc0db8b21b4aa53a5978d6139e CACHE INTERNAL "")
-
-#macosx 64
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_macosx_abi98_url https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=macosx-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_macosx_abi98_FOLDER ext-yaml-cpp-macosx-0.5.1-83d48489ea31426ab801fc0349310e63b1c6e3bf CACHE INTERNAL "")
-
-#linux64 CXX11
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_linux_abi11_URL https://gite.lirmm.fr/pid/ext-yaml-cpp/repository/archive.tar.gz?ref=linux-64-cxx11-0.5.1 CACHE INTERNAL "")
-set(yaml-cpp_REFERENCE_0.5.1_x86_64_linux_abi11_FOLDER ext-yaml-cpp-linux-64-cxx11-0.5.1-bb41bb231dd02d66da55cb874b1dd1d5ce51f57b CACHE INTERNAL "")
-
-
-
-
