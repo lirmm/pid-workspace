88eeca0c8df245e775ec00d34d409b29fa40e0bd
Robin Passama <robin.passama@lirmm.fr>
2017-09-13 11:00:09 +0200

    updated find file for yaml-cpp external package with new version managed

--- share/cmake/find/Findyaml-cpp.cmake
+++ share/cmake/find/Findyaml-cpp.cmake
@@ -1,3 +1,4 @@
-set(yaml-cpp_PID_KNOWN_VERSION 0.5.1)
-#set(yaml-cpp_PID_KNOWN_VERSION_0.5.1_GREATER_VERSIONS_COMPATIBLE_UP_TO 1.90)#the 1.90 is the first version that is not compatible with 1.55 version !!
+set(yaml-cpp_PID_KNOWN_VERSION 0.5.1 0.5.3)
+set(yaml-cpp_PID_KNOWN_VERSION_0.5.1_GREATER_VERSIONS_COMPATIBLE_UP_TO 0.6)
+set(yaml-cpp_PID_KNOWN_VERSION_0.5.3_GREATER_VERSIONS_COMPATIBLE_UP_TO 0.6)
 finding_External_Package(yaml-cpp)
