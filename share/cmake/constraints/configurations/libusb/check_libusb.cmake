f942578aad34a9713b37c1b8dbe6756aaf06e2c5
Robin Passama <robin.passama@lirmm.fr>
2018-01-16 11:03:29 +0100

    adding configuration files for libusb 1.0

---
+++ share/cmake/constraints/configurations/libusb/check_libusb.cmake
@@ -0,0 +1,34 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supporting the PID methodology              	#
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+if(NOT libusb_FOUND) #any linux or macosx is zlib ...
+	set(libusb_COMPILE_OPTIONS CACHE INTERNAL "")
+	set(libusb_INCLUDE_DIRS CACHE INTERNAL "")
+	set(libusb_LINK_OPTIONS CACHE INTERNAL "")
+	set(libusb_RPATH CACHE INTERNAL "")
+	include(${WORKSPACE_DIR}/share/cmake/constraints/configurations/libusb/find_libusb.cmake)
+	if(libusb_FOUND)
+		set(libusb_LINK_OPTIONS ${libusb_LIBRARIES} CACHE INTERNAL "") #simply adding all libusb standard libraries
+		set(CHECK_libusb_RESULT TRUE)
+	else()
+		set(CHECK_libusb_RESULT FALSE)
+	endif()
+else()
+	set(CHECK_libusb_RESULT TRUE)
+endif()
---
+++ share/cmake/constraints/configurations/libusb/find_libusb.cmake
@@ -0,0 +1,46 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+set(libusb_FOUND FALSE CACHE INTERNAL "")
+# - Find libusb installation
+# Try to find libraries for libusb on UNIX systems. The following values are defined
+#  libusb_FOUND        - True if libusb is available
+#  libusb_LIBRARIES    - link against these to use libusb library
+if (UNIX)
+
+	find_path(libusb_INCLUDE_PATH libusb-1.0/libusb.h)#find the path of the library
+	find_library(libusb_LIB usb-1.0)
+
+	set(libusb_LIBRARIES) # start with empty list
+	set(IS_FOUND TRUE)
+	if(libusb_INCLUDE_PATH AND libusb_LIB)
+		set(libusb_LIBRARIES -lusb-1.0)
+	else()
+		message("[PID] ERROR : cannot find usb1.0 library.")
+		set(IS_FOUND FALSE)
+	endif()
+
+	if(IS_FOUND)
+		set(libusb_FOUND TRUE CACHE INTERNAL "")
+	endif ()
+
+	unset(IS_FOUND)
+	unset(libusb_INCLUDE_PATH CACHE)
+	unset(libusb_LIB CACHE)
+endif ()
