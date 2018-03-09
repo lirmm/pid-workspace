0f3de66504c45a2eff120f2787d3f3404039e072
Robin Passama <robin.passama@lirmm.fr>
2018-03-09 11:58:58 +0100

    adding new generated references to opencv

--- share/cmake/find/Findopencv.cmake
+++ share/cmake/find/Findopencv.cmake
@@ -1,3 +1,3 @@
 set(opencv_PID_KNOWN_VERSION 2.4.11)
-#set(opencv_PID_KNOWN_VERSION_1.55.0_GREATER_VERSIONS_COMPATIBLE_UP_TO 1.90)#the 1.90 is the first version that is not compatible with 1.55 version !!
+
 finding_External_Package(opencv)
--- share/cmake/references/ReferExternalopencv.cmake
+++ share/cmake/references/ReferExternalopencv.cmake
@@ -1,25 +1,20 @@
-#### referencing external package opencv ####
+#### referencing wrapper of external package opencv ####
 set(opencv_PID_WRAPPER_CONTACT_AUTHOR _Robin_Passama CACHE INTERNAL "")
-set(opencv_PID_WRAPPER_CONTACT_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_CONTACT_INSTITUTION _CNRS_/_LIRMM:_Laboratoire_d'Informatique_de_Robotique_et_de_Microélectronique_de_Montpellier,_www.lirmm.fr CACHE INTERNAL "")
 set(opencv_PID_WRAPPER_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
-set(opencv_AUTHORS "OpenCV.org authors, see http://www.opencv.org" CACHE INTERNAL "")
-set(opencv_LICENSES "3-clause BSD License" CACHE INTERNAL "")
-set(opencv_DESCRIPTION external package providing C++ libraries for computer vision, repackaged for PID CACHE INTERNAL "")
-set(opencv_CATEGORIES programming/image vision CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_SITE_ROOT_PAGE  CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_PROJECT_PAGE https://gite.lirmm.fr/pid/opencv CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_SITE_GIT_ADDRESS  CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_SITE_INTRODUCTION This;project;is;a;PID;wrapper;for;the;external;project;called;opencv.;opencv;provides;many;libraries;used;for;image;processing. CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_AUTHORS_AND_INSTITUTIONS "_Robin_Passama(_CNRS_/_LIRMM:_Laboratoire_d'Informatique_de_Robotique_et_de_Microélectronique_de_Montpellier,_www.lirmm.fr)" CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_YEARS 2018 CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_LICENSE BSD CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_ADDRESS git@gite.lirmm.fr:pid/opencv.git CACHE INTERNAL "")
+set(opencv_PID_WRAPPER_PUBLIC_ADDRESS https://gite.lirmm.fr/pid/opencv.git CACHE INTERNAL "")
+set(opencv_DESCRIPTION "PID wrapper for opencv external project" CACHE INTERNAL "")
 set(opencv_FRAMEWORK pid CACHE INTERNAL "")
-
-# declaration of known references
-set(opencv_REFERENCES 2.4.11 CACHE INTERNAL "")
-set(opencv_REFERENCE_2.4.11 x86_64_linux_abi11 x86_32_linux_abi98 x86_64_linux_abi98 CACHE INTERNAL "")
-
-#x86_32_linux_abi98
-set(opencv_REFERENCE_2.4.11_x86_32_linux_abi98_URL https://gite.lirmm.fr/pid/ext-opencv/repository/archive.tar.gz?ref=linux-32-2.4.11 CACHE INTERNAL "")
-set(opencv_REFERENCE_2.4.11_x86_32_linux_abi98_FOLDER ext-opencv-linux-32-2.4.11-691f1f1dfefa162bb643b81f6362ef53f02efa2e CACHE INTERNAL "")
-
-#x86_64_linux_abi98
-set(opencv_REFERENCE_2.4.11_x86_64_linux_abi98_URL https://gite.lirmm.fr/pid/ext-opencv/repository/archive.tar.gz?ref=linux-64-2.4.11 CACHE INTERNAL "")
-set(opencv_REFERENCE_2.4.11_x86_64_linux_abi98_FOLDER ext-opencv-linux-64-2.4.11-3438bee806663c2d17517c40f26d17d506b61298 CACHE INTERNAL "")
-
-#x86_64_linux_abi11
-set(opencv_REFERENCE_2.4.11_x86_64_linux_abi11_URL https://gite.lirmm.fr/pid/ext-opencv/repository/archive.tar.gz?ref=linux-64-cxx11-2.4.11 CACHE INTERNAL "")
-set(opencv_REFERENCE_2.4.11_x86_64_linux_abi11_FOLDER ext-opencv-linux-64-cxx11-2.4.11-ecd63ef3a7492839c460a41d0b0cf06551ca2608 CACHE INTERNAL "")
+set(opencv_CATEGORIES "programming/image" CACHE INTERNAL "")
+set(opencv_AUTHORS "opencv.org members" CACHE INTERNAL "")
+set(opencv_PROJECT_SITE http://www.opencv.org CACHE INTERNAL "")
+set(opencv_LICENSES 3-clause BSD License CACHE INTERNAL "")
+set(opencv_REFERENCES  CACHE INTERNAL "")
