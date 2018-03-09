54a9c6fbd3c43c7fe6b23b067b2d5405d60d6deb
Robin Passama <robin.passama@lirmm.fr>
2018-03-09 10:50:51 +0100

    solved BUG with wrapper sites generation

--- share/cmake/system/api/PID_Documentation_Management_Functions.cmake
+++ share/cmake/system/api/PID_Documentation_Management_Functions.cmake
@@ -457,8 +457,14 @@ endif()
 
 if(NOT PACKAGE_DEPENDENCIES_DESCRIPTION) #means that the package has dependencies
 	foreach(dep_package IN LISTS ${PROJECT_NAME}_KNOWN_VERSION_${PACKAGE_LAST_VERSION_WITH_PATCH}_DEPENDENCIES)# we take only dependencies of the last version
-		generate_External_Dependency_Site(${dep_package} "${${PROJECT_NAME}_KNOWN_VERSION_${PACKAGE_LAST_VERSION_WITH_PATCH}_DEPENDENCY_${dep_package}_VERSION_USED_FOR_BUILD}" RES_CONTENT_EXTERNAL)
-		set(EXTERNAL_SITE_SECTION "${EXTERNAL_SITE_SECTION}\n${RES_CONTENT_EXTERNAL}")
+    set(prefix ${PROJECT_NAME}_KNOWN_VERSION_${PACKAGE_LAST_VERSION_WITH_PATCH}_DEPENDENCY_${dep_package})
+    list(FIND  ${prefix}_VERSIONS_EXACT ${${prefix}_VERSION_USED_FOR_BUILD} INDEX)
+    if(INDEX EQUAL -1)#not an exact version
+      generate_External_Dependency_Site(${dep_package} "${${prefix}_VERSION_USED_FOR_BUILD}" FALSE RES_CONTENT_EXTERNAL)
+    else()
+      generate_External_Dependency_Site(${dep_package} "${${prefix}_VERSION_USED_FOR_BUILD}" TRUE RES_CONTENT_EXTERNAL)
+    endif()
+    set(EXTERNAL_SITE_SECTION "${EXTERNAL_SITE_SECTION}\n${RES_CONTENT_EXTERNAL}")
 	endforeach()
 	set(PACKAGE_DEPENDENCIES_DESCRIPTION "${EXTERNAL_SITE_SECTION}\n")
 endif()
