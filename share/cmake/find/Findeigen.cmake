65cfdc6ae3439e8646d6cd460cb03783db99c647
Robin Passama <robin.passama@lirmm.fr>
2018-03-12 16:06:33 +0100

    updating references of eigen (now generated from its wrapper)

--- share/cmake/find/Findeigen.cmake
+++ share/cmake/find/Findeigen.cmake
@@ -1,3 +1,5 @@
-set(eigen_PID_KNOWN_VERSION 3.2.0 3.2.9)
-set(eigen_PID_KNOWN_VERSION_3.2.0_GREATER_VERSIONS_COMPATIBLE_UP_TO 3.3.0) #the 3.3.0 is the first version that is not compatible with 3.2.0 version !!
+set(eigen_PID_KNOWN_VERSION 3.2.0;3.2.9;3.3.4)
+
+set(eigen_PID_KNOWN_VERSION_3.2.0_GREATER_VERSIONS_COMPATIBLE_UP_TO 100000.100000.100000)
+set(eigen_PID_KNOWN_VERSION_3.2.9_GREATER_VERSIONS_COMPATIBLE_UP_TO 100000.100000.100000)
 finding_External_Package(eigen)
