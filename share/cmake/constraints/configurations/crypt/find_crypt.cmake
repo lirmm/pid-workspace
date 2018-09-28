57e688100305edb3fa23d660b8b7fd9a857149f2
Charles Villard <charles.villard@epita.fr>
2018-09-28 13:13:08 +0200

    Add: configurations: Add the crypt library platform configuration
    (cherry picked from commit 386fad06d65fbe6c2134d24e621c5d1ba5c9c24b)

---
+++ share/cmake/constraints/configurations/crypt/find_crypt.cmake
@@ -0,0 +1,59 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+set(crypt_FOUND FALSE CACHE INTERNAL "")
+# - Find crypt installation
+# Try to find libraries for crypt on UNIX systems. The following values are defined
+#  crypt_FOUND        - True if posix is available
+#  crypt_LIBRARIES    - link against these to use posix system
+if (UNIX)
+
+	# posix is never a framework and some header files may be
+	# found in tcl on the mac
+	set(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
+	set(CMAKE_FIND_FRAMEWORK NEVER)
+
+	find_path(crypt_INCLUDE_PATH crypt.h
+	          /usr/local/include/crypt
+		  /usr/local/include
+		  /usr/include/crypt
+		  /usr/include)
+
+	find_library(crypt_LIB
+		NAMES crypt
+		PATHS /usr/local/lib /usr/lib /lib )
+
+	set(crypt_LIBRARIES) # start with empty list
+	set(IS_FOUND TRUE)
+	if(crypt_INCLUDE_PATH AND crypt_LIB)
+		set(crypt_LIBRARIES ${crypt_LIBRARIES} -lcrypt)
+	else()
+		message("[PID] ERROR : cannot find crypt library.")
+		set(IS_FOUND FALSE)
+	endif()
+
+	if(IS_FOUND)
+		set(crypt_FOUND TRUE CACHE INTERNAL "")
+	endif ()
+
+	unset(IS_FOUND)
+	unset(crypt_INCLUDE_PATH CACHE)
+	unset(crypt_LIB CACHE)
+	set(CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_SAVE})
+endif ()
