0fe8cf6723f2724b111ca940d94a91ab3a1e135e
Robin Passama <robin.passama@lirmm.fr>
2018-10-10 12:22:32 +0200

    adding turbojpeg configuration

---
+++ share/cmake/constraints/configurations/turbojpeg/find_turbojpeg.cmake
@@ -0,0 +1,37 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+set(turbojpeg_FOUND FALSE CACHE INTERNAL "")
+# - Find turbojpeg installation
+# Try to find libraries for turbojpeg on UNIX systems. The following values are defined
+#  turbojpeg_FOUND        - True if turbojpeg is available
+#  turbojpeg_LIBRARIES    - link against these to use turbojpeg library
+set(turbojpeg_LIBRARIES) # start with empty list
+
+find_path(TurboJPEG_INCLUDE_DIRS NAMES turbojpeg.h)
+find_library(TurboJPEG_LIBRARIES NAMES libturbojpeg.so.1 libturbojpeg.so.0) #either version 0 or 1 is used (depending on distro)
+
+if(TurboJPEG_INCLUDE_DIRS AND NOT TurboJPEG_INCLUDE_DIRS MATCHES TurboJPEG_INCLUDE_DIRS-NOTFOUND
+	AND TurboJPEG_LIBRARIES AND NOT TurboJPEG_LIBRARIES MATCHES TurboJPEG_LIBRARIES-NOTFOUND)
+	set(turbojpeg_LIBRARIES -lturbojpeg) # start with empty list
+	set(turbojpeg_FOUND TRUE CACHE INTERNAL "")
+
+	unset(TurboJPEG_INCLUDE_DIRS CACHE)
+	unset(TurboJPEG_LIBRARIES CACHE)
+endif()
---
+++ share/cmake/constraints/configurations/turbojpeg/install_turbojpeg.cmake
@@ -0,0 +1,43 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+include(${WORKSPACE_DIR}/share/cmake/constraints/configurations/turbojpeg/installable_turbojpeg.cmake)
+if(turbojpeg_INSTALLABLE)
+	message("[PID] INFO : trying to install turbojpeg...")
+	if(CURRENT_DISTRIBUTION STREQUAL ubuntu)
+		if(CURRENT_DISTRIBUTION_VERSION VERSION_LESS 17.10)
+			execute_process(COMMAND sudo apt-get install libturbojpeg libjpeg-turbo8-dev)
+		else()
+			execute_process(COMMAND sudo apt-get install libturbojpeg0-dev)
+		endif()
+	elseif(CURRENT_DISTRIBUTION STREQUAL debian)
+		execute_process(COMMAND sudo apt-get install libturbojpeg0-dev)
+	endif()
+
+	include(${WORKSPACE_DIR}/share/cmake/constraints/configurations/turbojpeg/find_turbojpeg.cmake)
+	if(turbojpeg_FOUND)
+		message("[PID] INFO : turbojpeg installed !")
+		set(turbojpeg_INSTALLED TRUE)
+	else()
+		set(turbojpeg_INSTALLED FALSE)
+		message("[PID] INFO : install of turbojpeg has failed !")
+	endif()
+else()
+	set(turbojpeg_INSTALLED FALSE)
+endif()
