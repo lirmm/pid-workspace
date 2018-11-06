d315ff3dafd9a8f371095c9edeae137759555e50
Robin Passama <robin.passama@lirmm.fr>
2018-11-06 17:29:33 +0100

    CUDA now based on config + workspace features

--- configurations/cuda/find_cuda.cmake
+++ configurations/cuda/find_cuda.cmake
@@ -24,14 +24,62 @@ set(cuda_FOUND FALSE CACHE INTERNAL "")
 #  cuda_LIBRARIES    - link against these to use cuda library
 
 set(CUDA_USE_STATIC_CUDA_RUNTIME FALSE CACHE INTERNAL "" FORCE)
+set(cuda_BINARY_CONSTRAINTS)
 if(CUDA_VERSION)#if the CUDA version is known (means that a nvcc compiler has been defined)
-	if(NOT cuda_architecture) #no architecture defined => take the default one
-		set(cuda_FOUND TRUE CACHE INTERNAL "")
-		set(USED_cuda_architecture ${DEFAULT_CUDA_ARCH} CACHE INTERNAL "")
-		#TODO detect current architecture
-	else() #there is a target architecture defined
-		#need check to check if architecture is compatible with nvcc compiler
-		set(USED_cuda_architecture ${cuda_architecture} CACHE INTERNAL "")
-		#TODO detect possible architecture for nvcc then compare
+	if(NOT cuda_architecture) #no target architecture defined => take the default one
+		list(APPEND cuda_BINARY_CONSTRAINTS "architecture=${DEFAULT_CUDA_ARCH}") #architecture is the only argument that must be constrained in the resulting binary
+	endif()#otherwise the constraint on architecture will exists beore calling the configuration
+
+	if(	NOT cuda_version														#no contraint on version
+			OR cuda_version VERSION_EQUAL CUDA_VERSION) #required VS provided CUDA version match !
+		set(adequate_version_found TRUE)
+	else()
+		set(adequate_version_found FALSE)
 	endif()
-endif()
+
+	if( NOT cuda_architecture                            #no contraint on architecture
+			OR cuda_architecture STREQUAL DEFAULT_CUDA_ARCH  #required VS provided CUDA architecture match !
+		)
+		set(arch_to_use ${DEFAULT_CUDA_ARCH})
+	else()#there is a target architecture defined but not the default one
+		list(FIND AVAILABLE_CUDA_ARCHS ${cuda_architecture} INDEX)
+		if(NOT INDEX EQUAL -1)#check if the target arch is a possible arch for NVCC compiler
+			list(GET AVAILABLE_CUDA_ARCHS ${INDEX} RES_ARCH) #TODO detect possible architecture for nvcc then compare
+			set(arch_to_use ${RES_ARCH})
+		else()
+			set(arch_to_use)
+		endif()
+	endif()
+
+	if(arch_to_use AND adequate_version_found)
+		set(cuda_FOUND TRUE CACHE INTERNAL "")
+		set(cuda_ARCHITECTURE ${arch_to_use} CACHE INTERNAL "")
+
+		# NVCC flags to be set
+		set(NVCC_FLAGS_EXTRA "")
+		set(CUDA_ARCH_BIN "")
+		set(CUDA_ARCH_FEATURES "")
+
+		string(REGEX REPLACE "\\." "" ARCH_BIN_NO_POINTS "${arch_to_use}")
+		string(REGEX MATCHALL "[0-9()]+" ARCH_LIST "${ARCH_BIN_NO_POINTS}")
+
+		# Tell NVCC to add binaries for the specified GPUs
+		foreach(arch IN LISTS ARCH_LIST)
+		  if(arch MATCHES "([0-9]+)\\(([0-9]+)\\)")
+		    # User explicitly specified PTX for the concrete BIN
+		    set(NVCC_FLAGS_EXTRA ${NVCC_FLAGS_EXTRA} -gencode arch=compute_${CMAKE_MATCH_2},code=sm_${CMAKE_MATCH_1})
+		    set(CUDA_ARCH_BIN "${CUDA_ARCH_BIN} ${CMAKE_MATCH_1}"  CACHE INTERNAL "")
+		    set(CUDA_ARCH_FEATURES "${CUDA_ARCH_FEATURES} ${CMAKE_MATCH_2}"  CACHE INTERNAL "")
+		  else()
+		    # User didn't explicitly specify PTX for the concrete BIN, we assume PTX=BIN
+		    set(NVCC_FLAGS_EXTRA ${NVCC_FLAGS_EXTRA} -gencode arch=compute_${ARCH},code=sm_${ARCH})
+		    set(CUDA_ARCH_BIN "${CUDA_ARCH_BIN} ${ARCH}"  CACHE INTERNAL "")
+		    set(CUDA_ARCH_FEATURES "${CUDA_ARCH_FEATURES} ${ARCH}"  CACHE INTERNAL "")
+		  endif()
+		endforeach()
+		set(NVCC_FLAGS_EXTRA ${NVCC_FLAGS_EXTRA} -D_FORCE_INLINES)
+		# These vars will be processed in other scripts
+		set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} ${NVCC_FLAGS_EXTRA} CACHE INTERNAL "" FORCE)
+		set(CMAKE_CUDA_FLAGS ${CUDA_NVCC_FLAGS} CACHE INTERNAL "" FORCE)
+	endif()
+endif()#if NVCC not found no need to continue
--- share/cmake/platforms/CheckCUDA.cmake
+++ share/cmake/platforms/CheckCUDA.cmake
@@ -9,6 +9,7 @@ endif()
 
 find_package(CUDA)
 if(CUDA_FOUND)#simply stop the configuration
+  #setting general variables
   set(CUDA_Language_AVAILABLE TRUE CACHE INTERNAL "")
   #memorizing build variables
   set(CUDA_LIBRARIES ${CUDA_LIBRARIES} CACHE INTERNAL "")
@@ -18,6 +19,33 @@ if(CUDA_FOUND)#simply stop the configuration
   else()
     set(CUDA_CMAKE_SUPPORT "NEW" CACHE INTERNAL "")
     set(CMAKE_CUDA_COMPILER ${CUDA_NVCC_EXECUTABLE} CACHE FILEPATH "")
-    enable_language(CUDA) #enable FORTRAN language will generate appropriate variables
+    enable_language(CUDA) #enable CUDA language will generate appropriate variables
   endif()
+  set(__cuda_arch_bin)
+  set(__cuda_arch_ptx)
+
+  #TODO improve this check now only based on CUDA version
+  if(CUDA_VERSION VERSION_LESS "9.0")
+    set(AVAILABLE_CUDA_ARCHS "2.0" "3.0" "3.5" "3.7" "5.0" "5.2" "6.0" "6.1" CACHE INTERNAL "")
+  else()
+    set(AVAILABLE_CUDA_ARCHS "3.0" "3.5" "3.7" "5.0" "5.2" "6.0" "6.1" "7.0" CACHE INTERNAL "")
+  endif()
+
+  #detecting current CUDA architecture
+  execute_process( COMMAND "${CUDA_NVCC_EXECUTABLE}" ${CUDA_NVCC_FLAGS} "${WORKSPACE_DIR}/share/cmake/platforms/DetectCudaArch.cu" "--run"
+                     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+                     RESULT_VARIABLE _nvcc_res OUTPUT_VARIABLE _nvcc_out
+                     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(_nvcc_res EQUAL 0)#OK default arch has been found
+    string(REPLACE "2.1" "2.1(2.0)" _nvcc_out "${_nvcc_out}")
+    set(__cuda_arch_bin "${_nvcc_out}")
+    set(DEFAULT_CUDA_ARCH ${__cuda_arch_bin} CACHE INTERNAL "")
+  else()# choose the default arch among those available (take the greatest version)
+    if(CUDA_VERSION VERSION_LESS "9.0")
+      set(DEFAULT_CUDA_ARCH "6.1" CACHE INTERNAL "")
+    else()
+      set(DEFAULT_CUDA_ARCH "7.0" CACHE INTERNAL "")
+    endif()
+  endif()
+
 endif()
