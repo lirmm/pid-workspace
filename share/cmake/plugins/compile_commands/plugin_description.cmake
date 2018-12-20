419bb3150a838fee90ee9d2622adc3112ec3d7cf
Charles Villard <charles.villard@epita.fr>
2018-12-20 10:05:45 +0100

    add: plugins: Add the compile commands plugin to enable the generation of the compile_command.json file by cmake

---
+++ share/cmake/plugins/compile_commands/plugin_description.cmake
@@ -0,0 +1,24 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+set(compile_commands_PLUGIN_DESCRIPTION "use this plugin to generate compile commands configuration files to be used to get auto completion and in editor errors for your favorite editor" CACHE INTERNAL "")
+
+set(compile_commands_PLUGIN_ACTIVATION_MESSAGE "plugin compile_commands: generating compile_commands.json configuration file" CACHE INTERNAL "")
+
+set(compile_commands_PLUGIN_RESIDUAL_FILES "compile_commands.json" CACHE INTERNAL "")
