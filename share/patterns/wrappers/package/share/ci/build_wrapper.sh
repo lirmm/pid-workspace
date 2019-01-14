5a8ce8aade1df75ed3986a35095057de37c6d986
Robin Passama <robin.passama@lirmm.fr>
2019-01-14 10:21:23 +0100

    fix bug: no binary archive generated by wrapper on CI builds (see #109).

--- share/patterns/wrappers/package/share/ci/build_wrapper.sh
+++ share/patterns/wrappers/package/share/ci/build_wrapper.sh
@@ -2,4 +2,14 @@
 #!/bin/bash
 
 # build the site
-cd build && version=$1 cmake --build . --target build && cd ..
+cd build
+echo "[PID] CI : generating the binary archive..."
+
+if [ "$PACKAGE_BINARIES_PUBLISHED" = true ]; then
+  # if wrapper publishes binaries then we need to specifically configure archives generation during the build
+  version=$1 archive=true cmake --build . --target build
+else
+  version=$1 cmake --build . --target build
+fi
+
+cd ..
