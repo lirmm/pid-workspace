07bf8ff594e1ec49d088351cc62856294fd01098
Robin Passama <robin.passama@lirmm.fr>
2019-01-15 18:54:57 +0100

    fix bug in remove command

--- share/cmake/system/commands/Remove_PID_Package.cmake
+++ share/cmake/system/commands/Remove_PID_Package.cmake
@@ -39,7 +39,7 @@ if(TARGET_PACKAGE OR ENV{package})
 		remove_PID_Package(${TARGET_PACKAGE})
 	elseif(EXISTS ${WORKSPACE_DIR}/wrappers/${TARGET_PACKAGE}
 		AND IS_DIRECTORY ${WORKSPACE_DIR}/wrappers/${TARGET_PACKAGE})
-		remove_PID_Wrapper(${TARGET_PACKAGE})()
+		remove_PID_Wrapper(${TARGET_PACKAGE})
 		message("[PID] ERROR : the package to be removed, named ${TARGET_PACKAGE}, does not lies in the workspace.")
 	endif()
 elseif(TARGET_FRAMEWORK)
