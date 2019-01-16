c6decc0a3065b7d0fb8706f7ae504c63a63efec6
Robin Passama <robin.passama@lirmm.fr>
2019-01-16 17:50:50 +0100

    changing references to vimba : now based on a wrapper of vimba binaries.

--- share/cmake/find/Findvimba.cmake
+++ share/cmake/find/Findvimba.cmake
@@ -1,4 +1,3 @@
-set(vimba_PID_KNOWN_VERSION 1.3.0)
-#set(vimba_PID_KNOWN_VERSION_1.3.0_GREATER_VERSIONS_COMPATIBLE_UP_TO 1.50)#the 1.50 is the first version that is not compatible with 1.3 version !!
-finding_External_Package(vimba)
+set(vimba_PID_KNOWN_VERSION 1.3.0;2.1.3)
 
+finding_External_Package(vimba)
--- share/cmake/references/ReferExternalvimba.cmake
+++ share/cmake/references/ReferExternalvimba.cmake
@@ -1,25 +1,20 @@
-#### referencing external package vimba ####
+#### referencing wrapper of external package vimba ####
 set(vimba_PID_WRAPPER_CONTACT_AUTHOR _Robin_Passama CACHE INTERNAL "")
-set(vimba_PID_WRAPPER_CONTACT_INSTITUTION _LIRMM: Laboratoire d'Informatique de Robotique et de Microélectronique de Montpellier, www.lirmm.fr CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_CONTACT_INSTITUTION _CNRS/LIRMM CACHE INTERNAL "")
 set(vimba_PID_WRAPPER_CONTACT_MAIL passama@lirmm.fr CACHE INTERNAL "")
-set(vimba_AUTHORS "Allied Vision Technology Gmbh, see http://www.alliedvision.com/" CACHE INTERNAL "")
-set(vimba_LICENSES "Allied Vision Technology license for Vimba" CACHE INTERNAL "")
-set(vimba_DESCRIPTION "external package providing driver for AVT cameras" CACHE INTERNAL "")
-set(vimba_CATEGORIES camera/avt CACHE INTERNAL "")
-
-# declaration of known references
-set(vimba_REFERENCES 1.3.0 CACHE INTERNAL "")
-set(vimba_REFERENCE_1.3.0 x86_64_linux_abi11 x86_32_linux_abi98 x86_64_linux_abi98 CACHE INTERNAL "")
-
-# x86_32_linux_abi98
-set(vimba_REFERENCE_1.3.0_x86_32_linux_abi98_URL https://gite.lirmm.fr/rob-vision-devices/ext-vimba/repository/linux-32-1.3.0/archive.tar.gz CACHE INTERNAL "")
-set(vimba_REFERENCE_1.3.0_x86_32_linux_abi98_FOLDER ext-vimba-linux-32-1.3.0-211b68c23102c1dec1d9c90ea83a29de6c395514 CACHE INTERNAL "")
-
-# x86_64_linux_abi98
-set(vimba_REFERENCE_1.3.0_x86_64_linux_abi98_URL https://gite.lirmm.fr/rob-vision-devices/ext-vimba/repository/linux-64-1.3.0/archive.tar.gz CACHE INTERNAL "")
-set(vimba_REFERENCE_1.3.0_x86_64_linux_abi98_FOLDER ext-vimba-linux-64-1.3.0-7c17b5404398d08371283f93aadd427f8c81a44b CACHE INTERNAL "")
-
-# x86_64_linux_abi11
-
-set(vimba_REFERENCE_1.3.0_x86_64_linux_abi11_URL https://gite.lirmm.fr/rob-vision-devices/ext-vimba/repository/linux-64-cxx11-1.3.0/archive.tar.gz CACHE INTERNAL "")
-set(vimba_REFERENCE_1.3.0_x86_64_linux_abi11_FOLDER ext-vimba-linux-64-cxx11-1.3.0-b56031dbc0ceec540f6c8b637416d43d9eb68c17 CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_SITE_ROOT_PAGE  CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_PROJECT_PAGE  CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_SITE_GIT_ADDRESS  CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_SITE_INTRODUCTION  CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_AUTHORS_AND_INSTITUTIONS "_Robin_Passama(_CNRS/LIRMM)" CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_YEARS 2019 CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_LICENSE GNULGPL CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_ADDRESS git@gite.lirmm.fr:rob-vision-devices/vimba.git CACHE INTERNAL "")
+set(vimba_PID_WRAPPER_PUBLIC_ADDRESS https://gite.lirmm.fr/rob-vision-devices/vimba.git CACHE INTERNAL "")
+set(vimba_DESCRIPTION "PID wrapper for the vimba project, developped by Allied Vision Technology company. Provides API to interface with various cameras from AVT." CACHE INTERNAL "")
+set(vimba_FRAMEWORK  CACHE INTERNAL "")
+set(vimba_CATEGORIES CACHE INTERNAL "")
+set(vimba_AUTHORS "Allied Vision Technology Gmbh" CACHE INTERNAL "")
+set(vimba_PROJECT_SITE https://www.alliedvision.com CACHE INTERNAL "")
+set(vimba_LICENSES Allied Vision Technology license for Vimba CACHE INTERNAL "")
+set(vimba_REFERENCES  CACHE INTERNAL "")
