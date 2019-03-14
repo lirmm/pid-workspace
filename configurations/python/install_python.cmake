193e8a9f412c68a36f47e9c28b19b7c83cc35d69
Robin Passama <robin.passama@lirmm.fr>
2019-03-14 11:32:13 +0100

    adding configruation to check python properties

---
+++ configurations/python/install_python.cmake
@@ -0,0 +1,37 @@
+#########################################################################################
+#       This file is part of the program PID                                            #
+#       Program description : build system supportting the PID methodology              #
+#       Copyright (C) Robin Passama, LIRMM (Laboratoire d'Informatique de Robotique     #
+#       et de Microelectronique de Montpellier). All Right reserved.                    #
+#                                                                                       #
+#       This software is free software: you can redistribute it and/or modify           #
+#       it under the terms of the CeCILL-C license as published by                      #
+#       the CEA CNRS INRIA, either version 1                                            #
+#       of the License, or (at your option) any later version.                          #
+#       This software is distributed in the hope that it will be useful,                #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    #
+#       CeCILL-C License for more details.                                              #
+#                                                                                       #
+#       You can find the complete license description on the official website           #
+#       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
+#########################################################################################
+
+if(python_version VERSION_LESS 3.0)
+	if(CURRENT_PYTHON VERSION_LESS python_version
+	OR NOT CURRENT_PYTHON VERSION_LESS 3.0)
+		return() #problem in version specification, cannot do mush more
+	endif()
+	set(PIP_TOOl pip2)
+else()
+	if(CURRENT_PYTHON VERSION_LESS python_version
+		OR CURRENT_PYTHON VERSION_LESS 3.0)#OK version match
+		return()
+	endif()
+	set(PIP_TOOl pip3)
+endif()
+
+#ok now installing all required packages
+foreach(pack IN LISTS python_packages)
+	execute_OS_Configuration_Command(${PIP_TOOl} install ${pack} OUTPUT_VARIABLE SHOW_OUTPUT)
+endforeach()
