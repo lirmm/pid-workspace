25b24422fbe5b325e1b10dce80f5ed7236e74b89
Robin Passama <robin.passama@lirmm.fr>
2019-03-22 18:07:02 +0100

    adding configuration for tiff

--- configurations/tiff/find_tiff.cmake
+++ configurations/tiff/find_tiff.cmake
@@ -16,3 +16,41 @@
 #       You can find the complete license description on the official website           #
 #       of the CeCILL licenses family (http://www.cecill.info/index.en.html)            #
 #########################################################################################
+
+include(Configuration_Definition NO_POLICY_SCOPE)
+
+found_PID_Configuration(tiff FALSE)
+
+if (UNIX)
+#copied stuff from official find tiff
+find_path(TIFF_INCLUDE_DIR tiff.h)
+
+set(TIFF_NAMES ${TIFF_NAMES} tiff libtiff tiff3 libtiff3)
+foreach(name ${TIFF_NAMES})
+  list(APPEND TIFF_NAMES_DEBUG "${name}d")
+endforeach()
+
+find_library(TIFF_LIBRARY NAMES ${TIFF_NAMES})
+mark_as_advanced(TIFF_LIBRARY)
+unset(TIFF_NAMES)
+unset(TIFF_NAMES_DEBUG)
+
+if(TIFF_INCLUDE_DIR AND EXISTS "${TIFF_INCLUDE_DIR}/tiffvers.h")
+    file(STRINGS "${TIFF_INCLUDE_DIR}/tiffvers.h" tiff_version_str
+         REGEX "^#define[\t ]+TIFFLIB_VERSION_STR[\t ]+\"LIBTIFF, Version .*")
+
+    string(REGEX REPLACE "^#define[\t ]+TIFFLIB_VERSION_STR[\t ]+\"LIBTIFF, Version +([^ \\n]*).*"
+           "\\1" TIFF_VERSION_STRING "${tiff_version_str}")
+    unset(tiff_version_str)
+endif()
+
+	if(TIFF_INCLUDE_DIR AND TIFF_LIBRARY)
+		convert_PID_Libraries_Into_System_Links(TIFF_LIBRARY TIFF_LINKS)#getting good system links (with -l)
+		convert_PID_Libraries_Into_Library_Directories(TIFF_LIBRARY TIFF_LIBDIRS)
+		found_PID_Configuration(tiff TRUE)
+	else()
+		message("[PID] ERROR : cannot find tiff library.")
+	endif()
+
+	unset(TIFF_FOUND)
+endif ()
