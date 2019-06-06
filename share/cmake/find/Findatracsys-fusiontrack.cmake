564a9391c1837514fbfe303f730d1aab9f9e2692
Robin Passama <robin.passama@lirmm.fr>
2019-06-06 12:02:49 +0200

    atracsys-fusiontrack registered

--- share/cmake/find/Findatracsys-fusiontrack.cmake
+++ share/cmake/find/Findatracsys-fusiontrack.cmake
@@ -1,4 +1,3 @@
-set(atracsys-fusiontrack_PID_KNOWN_VERSION 3.0.1)
-#set(boost_PID_KNOWN_VERSION_1.55.0_GREATER_VERSIONS_COMPATIBLE_UP_TO 1.90)#the 1.90 is the first version that is not compatible with 1.55 version !!
-finding_External_Package(atracsys-fusiontrack)
+set(atracsys-fusiontrack_PID_KNOWN_VERSION )
 
+finding_External_Package(atracsys-fusiontrack)
