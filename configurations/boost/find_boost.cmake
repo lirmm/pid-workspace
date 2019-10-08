389d9ffec7af5c2e3b4000310ad8cf8334ec0bfd
Robin Passama <robin.passama@lirmm.fr>
2019-10-08 18:05:25 +0200

    (Configuration) optimized boost configuration (avoid unwanted reconfigure between release and debug builds)

--- configurations/boost/find_boost.cmake
+++ configurations/boost/find_boost.cmake
@@ -23,25 +23,50 @@ found_PID_Configuration(boost FALSE)
 set(components_to_search system filesystem ${boost_libraries})#boost_libraries used to check that components that user wants trully exist
 list(REMOVE_DUPLICATES components_to_search)#using system and filesystem as these two libraries exist since early versions of boost
 
+set(path_test_boost ${WORKSPACE_DIR}/configurations/boost/test_boost/build/${CURRENT_PLATFORM})# each result is conditionate by the current platform in use
+
+if(EXISTS ${path_test_boost}/boost_config_vars.cmake)#file already computed
+	include(${path_test_boost}/boost_config_vars.cmake) #just to check that same version is required
+	if(Boost_Found)#optimization only possible if boost has been found
+		if(NOT boost_version #no specific version to search for
+			OR boost_version VERSION_EQUAL BOOST_VERSION)# or same version required and already found no need to regenerate
+			#now simply have to check that components to search exist
+			set(all_found TRUE)
+			foreach(a_component IN LISTS components_to_search)
+				list(FIND BOOST_COMPONENTS ${a_component} COMP_INDEX)
+				if(COMP_INDEX EQUAL -1)#not found
+					set(all_found FALSE)
+					break()
+				endif()
+			endforeach()
+			if(all_found)
+				convert_PID_Libraries_Into_System_Links(Boost_LIBRARIES BOOST_LINKS)#getting good system links (with -l)
+				convert_PID_Libraries_Into_Library_Directories(Boost_LIBRARIES BOOST_LIBRARY_DIRS)
+				found_PID_Configuration(boost TRUE)
+				unset(Boost_FOUND)
+				return()#exit without regenerating (avoid regenerating between debug and release builds due to generated file timestamp change)
+				#also an optimization avoid launching again and again boost config for each package build in debug and release modes (which is widely used)
+			endif()
+		endif()
+	endif()
+endif()
+#any other situation implies a new configuration of the test project
+
 set(calling_defs "-DBOOST_COMPONENTS_TO_SEARCH=${components_to_search}")
 if(boost_version)
 	set(calling_defs "-DBOOST_VERSION_TO_SEARCH=${boost_version} ${calling_defs}")
 endif()
+
 if(CMAKE_HOST_WIN32)#on a window host path must be resolved
 	separate_arguments(COMMAND_ARGS_AS_LIST WINDOWS_COMMAND "${calling_defs}")
 else()#if not on wondows use a UNIX like command syntac
 	separate_arguments(COMMAND_ARGS_AS_LIST UNIX_COMMAND "${calling_defs}")#always from host perpective
 endif()
 
-# execute separate project to extract datas
-set(path_test_boost ${WORKSPACE_DIR}/configurations/boost/test_boost/build)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${path_test_boost}
-								WORKING_DIRECTORY  ${WORKSPACE_DIR}/pid OUTPUT_QUIET)
-file(WRITE ${path_test_boost}/.gitignore "*\n")
-
-if(ADDITIONNAL_DEBUG_INFO)
-	message("[PID] INFO : performing tests for Boost ...")
+if(NOT EXISTS ${path_test_boost})
+	file(MAKE_DIRECTORY ${path_test_boost})
 endif()
+# execute separate project to extract datas
 execute_process(COMMAND ${CMAKE_COMMAND} ${COMMAND_ARGS_AS_LIST} ${WORKSPACE_DIR}/configurations/boost/test_boost/
 								WORKING_DIRECTORY ${path_test_boost} OUTPUT_QUIET)
 
@@ -65,8 +90,5 @@ if(NOT Boost_FOUND)#check failed : due to missing searched component !!
 endif()
 convert_PID_Libraries_Into_System_Links(Boost_LIBRARIES BOOST_LINKS)#getting good system links (with -l)
 convert_PID_Libraries_Into_Library_Directories(Boost_LIBRARIES BOOST_LIBRARY_DIRS)
-if(ADDITIONNAL_DEBUG_INFO)
-	message("[PID] INFO : OS configured with Boost !")
-endif()
 found_PID_Configuration(boost TRUE)
 unset(Boost_FOUND)
