debd2e665e8694d3a63293e0f4ad0c809dfb1db7
Robin Passama <robin.passama@lirmm.fr>
2019-10-08 18:31:31 +0200

    (Configuration) optimized openmp configuration (avoid unwanted reconfiguration between release and debug builds)

--- configurations/openmp/find_openmp.cmake
+++ configurations/openmp/find_openmp.cmake
@@ -19,52 +19,30 @@
 
 include(Configuration_Definition NO_POLICY_SCOPE)
 
-found_PID_Configuration(openmp FALSE)
-
-# FindOpenMP need to be use in a "non script" environment (try_compile function)
-# so we create a separate project (test_openmp) to generate usefull vars
-# and extract them in a file (openmp_config_vars.cmake)
-# Then read this file in our context.
-
-
-# execute separate project to extract datas
-set(path_test_openmp ${WORKSPACE_DIR}/configurations/openmp/test_openmp/build)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${path_test_openmp} OUTPUT_QUIET)
-file(WRITE ${path_test_openmp}/.gitignore "*\n")
-execute_process(COMMAND ${CMAKE_COMMAND} .. WORKING_DIRECTORY ${path_test_openmp} OUTPUT_QUIET)
-
-# Extract datas from openmpi_config_vars.cmake
-set(path_openmp_config_vars ${path_test_openmp}/openmp_config_vars.cmake )
-if(EXISTS ${path_openmp_config_vars} )
-  include(${path_openmp_config_vars})
-else()
-  return()
-endif()
-
-if(OpenMP_FOUND)
-	if(OpenMP_C_FOUND AND OpenMP_CXX_FOUND)
-		list(APPEND OpenMP_LIB_NAMES ${OpenMP_C_LIB_NAMES} ${OpenMP_CXX_LIB_NAMES})
+macro(set_openmp_variables)
+  if(OpenMP_C_FOUND AND OpenMP_CXX_FOUND)
+    list(APPEND OpenMP_LIB_NAMES ${OpenMP_C_LIB_NAMES} ${OpenMP_CXX_LIB_NAMES})
     if(OpenMP_LIB_NAMES)
-	     list(REMOVE_DUPLICATES OpenMP_LIB_NAMES)
+       list(REMOVE_DUPLICATES OpenMP_LIB_NAMES)
     endif()
-		list(APPEND OpenMP_RPATH ${OpenMP_C_LIBRARIES} ${OpenMP_CXX_LIBRARIES})
+    list(APPEND OpenMP_RPATH ${OpenMP_C_LIBRARIES} ${OpenMP_CXX_LIBRARIES})
     if(OpenMP_RPATH)
       list(REMOVE_DUPLICATES OpenMP_RPATH)
     endif()
-		list(APPEND OpenMP_COMPILER_OPTIONS ${OpenMP_C_FLAGS} ${OpenMP_CXX_FLAGS})
+    list(APPEND OpenMP_COMPILER_OPTIONS ${OpenMP_C_FLAGS} ${OpenMP_CXX_FLAGS})
     if(OpenMP_COMPILER_OPTIONS)
-	     list(REMOVE_DUPLICATES OpenMP_COMPILER_OPTIONS)
+       list(REMOVE_DUPLICATES OpenMP_COMPILER_OPTIONS)
     endif()
-		convert_PID_Libraries_Into_System_Links(OpenMP_RPATH OpenMP_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_RPATH OpenMP_LIBDIRS)
+    convert_PID_Libraries_Into_System_Links(OpenMP_RPATH OpenMP_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_RPATH OpenMP_LIBDIRS)
 
-		convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_C_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_C_LIBDIRS)
+    convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_C_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_C_LIBDIRS)
 
-		convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_CXX_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_CXX_LIBDIRS)
+    convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_CXX_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_CXX_LIBDIRS)
 
-		#Check OpenMP less version
+    #Check OpenMP less version
     if (OpenMP_C_VERSION AND OpenMP_CXX_VERSION) # check if version is set (cmake version <3.9 is not set)
       if(OpenMP_C_VERSION LESS_EQUAL OpenMP_CXX_VERSION)
         set(OpenMP_VERSION ${OpenMP_C_VERSION})
@@ -73,29 +51,29 @@ if(OpenMP_FOUND)
       endif()
     endif()
 
-	elseif(OpenMP_C_FOUND AND NOT OpenMP_CXX_FOUND)
-	# If only C is found
-		set(OpenMP_LIB_NAMES ${OpenMP_C_LIB_NAMES})
-		set(OpenMP_RPATH ${OpenMP_C_LIBRARIES})
-		set(OpenMP_COMPILER_OPTIONS ${OpenMP_C_FLAGS})
-		convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_LIBDIRS)
-		set(OpenMP_VERSION ${OpenMP_C_FOUND})
+  elseif(OpenMP_C_FOUND AND NOT OpenMP_CXX_FOUND)
+  # If only C is found
+    set(OpenMP_LIB_NAMES ${OpenMP_C_LIB_NAMES})
+    set(OpenMP_RPATH ${OpenMP_C_LIBRARIES})
+    set(OpenMP_COMPILER_OPTIONS ${OpenMP_C_FLAGS})
+    convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_LIBDIRS)
+    set(OpenMP_VERSION ${OpenMP_C_FOUND})
 
-		convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_C_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_C_LIBDIRS)
+    convert_PID_Libraries_Into_System_Links(OpenMP_C_LIBRARIES OpenMP_C_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_C_LIBRARIES OpenMP_C_LIBDIRS)
 
-	elseif(NOT OpenMP_C_FOUND AND OpenMP_CXX_FOUND)
-	# If only CXX is found
-		set(OpenMP_LIB_NAMES ${OpenMP_CXX_LIB_NAMES})
-		set(OpenMP_RPATH ${OpenMP_CXX_LIBRARIES})
-		set(OpenMP_COMPILER_OPTIONS ${OpenMP_CXX_FLAGS})
-		convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_LIBDIRS)
-		set(OpenMP_VERSION ${OpenMP_CXX_FOUND})
+  elseif(NOT OpenMP_C_FOUND AND OpenMP_CXX_FOUND)
+  # If only CXX is found
+    set(OpenMP_LIB_NAMES ${OpenMP_CXX_LIB_NAMES})
+    set(OpenMP_RPATH ${OpenMP_CXX_LIBRARIES})
+    set(OpenMP_COMPILER_OPTIONS ${OpenMP_CXX_FLAGS})
+    convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_LIBDIRS)
+    set(OpenMP_VERSION ${OpenMP_CXX_FOUND})
 
-		convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_CXX_LINKS)#getting good system links (with -l)
-		convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_CXX_LIBDIRS)
+    convert_PID_Libraries_Into_System_Links(OpenMP_CXX_LIBRARIES OpenMP_CXX_LINKS)#getting good system links (with -l)
+    convert_PID_Libraries_Into_Library_Directories(OpenMP_CXX_LIBRARIES OpenMP_CXX_LIBDIRS)
 
   elseif(NOT OpenMP_C_FOUND AND NOT OpenMP_CXX_FOUND )
   # If older version of cmake is used => OpenMP_<lang>_FOUND not exist
@@ -108,9 +86,9 @@ if(OpenMP_FOUND)
     find_library(OpenMP_PTHREAD_LIB NAMES libpthread pthread)
 
     set(OpenMP_GOMP_LIBRARY ${OpenMP_GOMP_LIB})
-  	set(OpenMP_PTHREAD_LIBRARY ${OpenMP_PTHREAD_LIB})
-  	unset(OpenMP_GOMP_LIB CACHE)
-  	unset(OpenMP_PTHREAD_LIB CACHE)
+    set(OpenMP_PTHREAD_LIBRARY ${OpenMP_PTHREAD_LIB})
+    unset(OpenMP_GOMP_LIB CACHE)
+    unset(OpenMP_PTHREAD_LIB CACHE)
 
     if (OpenMP_GOMP_LIBRARY)
       set(OpenMP_GOMP_NAME "gomp")
@@ -131,12 +109,55 @@ if(OpenMP_FOUND)
 
     unset(OpenMP_PTHREAD_NAME)
     unset(OpenMP_GOMP_NAME)
+  else()
+    message("[PID] ERROR : cannot find OpenMP library.")
+    return()
+  endif()
+endmacro(set_openmp_variables)
+
+found_PID_Configuration(openmp FALSE)
+
+# FindOpenMP need to be use in a "non script" environment (try_compile function)
+# so we create a separate project (test_openmp) to generate usefull vars
+# and extract them in a file (openmp_config_vars.cmake)
+# Then read this file in our context.
 
 
-	else()
-		message("[PID] ERROR : cannot find OpenMP library.")
-		return()
+set(path_test_openmp ${WORKSPACE_DIR}/configurations/openmp/test_openmp/build/${CURRENT_PLATFORM})
+set(path_openmp_config_vars ${path_test_openmp}/openmp_config_vars.cmake )
+
+if(EXISTS ${path_openmp_config_vars})#file already computed
+	include(${path_openmp_config_vars}) #just to check that same version is required
+	if(OpenMP_FOUND)#optimization only possible if openMP has been found
+		if(NOT openmp_version #no specific version to search for
+			OR openmp_version VERSION_EQUAL OpenMP_VERSION)# or same version required and already found no need to regenerate
+        set_openmp_variables()
+      	found_PID_Configuration(openmp TRUE)
+      return()#exit without regenerating (avoid regenerating between debug and release builds due to generated file timestamp change)
+			#also an optimization avoid launching again and again boost config for each package build in debug and release modes (which is widely used)
+		endif()
 	endif()
+endif()
 
+# execute separate project to extract datas
+if(NOT EXISTS ${path_test_openmp})
+	file(MAKE_DIRECTORY ${path_test_openmp})
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} ${WORKSPACE_DIR}/configurations/openmp/test_openmp/
+                WORKING_DIRECTORY ${path_test_openmp} OUTPUT_QUIET)
+
+# Extract datas from openmpi_config_vars.cmake
+if(EXISTS ${path_openmp_config_vars} )
+  include(${path_openmp_config_vars})
+else()
+  if(ADDITIONNAL_DEBUG_INFO)
+		message("[PID] WARNING : cannot execute tests for OpenMP !")
+	endif()
+  return()
+endif()
+
+if(OpenMP_FOUND)
+  set_openmp_variables()
 	found_PID_Configuration(openmp TRUE)
 endif()
