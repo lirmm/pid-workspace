a3e4609df5fda221fe93aadbd9a3ffb8a392d540
Robin Passama <robin.passama@lirmm.fr>
2019-10-08 18:45:00 +0200

    (Configuration) optimized openmpi to avoid unwanted reconfiguration between release and debug builds

--- configurations/openmpi/find_openmpi.cmake
+++ configurations/openmpi/find_openmpi.cmake
@@ -19,31 +19,35 @@
 
 include(Configuration_Definition NO_POLICY_SCOPE)
 
-found_PID_Configuration(openmpi FALSE)
+macro(set_openmpi_version)
+  #need to extract OpenMPI version
+  find_path(OMPI_INCLUDE_DIR NAMES mpi.h PATHS ${MPI_C_INCLUDE_DIRS})
+  file(READ ${OMPI_INCLUDE_DIR}/mpi.h OMPI_VERSION_FILE_CONTENTS)
+  string(REGEX MATCH "define OMPI_MAJOR_VERSION * +([0-9]+)"
+        OMPI_MAJOR_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
+  string(REGEX REPLACE "define OMPI_MAJOR_VERSION * +([0-9]+)" "\\1"
+        OMPI_MAJOR_VERSION "${OMPI_MAJOR_VERSION}")
+  string(REGEX MATCH "define OMPI_MINOR_VERSION * +([0-9]+)"
+        OMPI_MINOR_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
+  string(REGEX REPLACE "define OMPI_MINOR_VERSION * +([0-9]+)" "\\1"
+        OMPI_MINOR_VERSION "${OMPI_MINOR_VERSION}")
+  string(REGEX MATCH "define OMPI_RELEASE_VERSION * +([0-9]+)"
+        OMPI_PATCH_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
+  string(REGEX REPLACE "define OMPI_RELEASE_VERSION * +([0-9]+)" "\\1"
+        OMPI_PATCH_VERSION "${OMPI_PATCH_VERSION}")
+  set(OMPI_VERSION ${OMPI_MAJOR_VERSION}.${OMPI_MINOR_VERSION}.${OMPI_PATCH_VERSION})
 
-# findMPI need to be use in a "non script" environment (try_compile function)
-# so we create a separate project (test_mpi) to generate usefull vars and extract them in a file (openmpi_config_vars.cmake)
-# and read this file in our context.
+  #Check MPI less version
+  if (MPI_C_VERSION AND MPI_CXX_VERSION) # check if version is set (cmake version <3.10 is not set)
+    if(MPI_C_VERSION LESS_EQUAL MPI_CXX_VERSION)
+      set(MPI_VERSION ${MPI_C_VERSION})
+    else()
+      set(MPI_VERSION ${MPI_CXX_VERSION})
+    endif()
+  endif()
+endmacro(set_openmpi_version)
 
-
-
-# execute separate project to extract datas
-set(path_test_mpi ${WORKSPACE_DIR}/configurations/openmpi/test_mpi/build)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${path_test_mpi} OUTPUT_QUIET)
-file(WRITE ${path_test_mpi}/.gitignore "*\n")
-execute_process(COMMAND ${CMAKE_COMMAND} .. WORKING_DIRECTORY ${path_test_mpi} OUTPUT_QUIET)
-
-# Extract datas from openmpi_config_vars.cmake
-set(path_mpi_config_vars ${path_test_mpi}/openmpi_config_vars.cmake )
-if(EXISTS ${path_mpi_config_vars} )
-  include(${path_mpi_config_vars})
-else()
-  message("[PID] ERROR : cannot find OpenMPI library (openmpi_config_vars.cmake not found).")
-  return()
-endif()
-
-if(MPI_C_FOUND AND MPI_CXX_FOUND)
-
+macro(set_openmpi_variables)
   # Extract and make list with all links Flags
   convert_PID_Libraries_Into_System_Links(MPI_C_LIBRARIES MPI_C_LINKS)#getting good system links (with -l)
   list(APPEND MPI_C_LINKS "-lpthread") #don't add MPI_C_LINK_FLAGS (not used by PID)
@@ -84,33 +88,52 @@ if(MPI_C_FOUND AND MPI_CXX_FOUND)
   if(MPI_LIBRARY_DIRS)
     list(REMOVE_DUPLICATES MPI_LIBRARY_DIRS)
   endif()
+endmacro(set_openmpi_variables)
 
-  #need to extract OpenMPI version
-  find_path(OMPI_INCLUDE_DIR NAMES mpi.h PATHS ${MPI_C_INCLUDE_DIRS})
-  file(READ ${OMPI_INCLUDE_DIR}/mpi.h OMPI_VERSION_FILE_CONTENTS)
-  string(REGEX MATCH "define OMPI_MAJOR_VERSION * +([0-9]+)"
-        OMPI_MAJOR_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
-  string(REGEX REPLACE "define OMPI_MAJOR_VERSION * +([0-9]+)" "\\1"
-        OMPI_MAJOR_VERSION "${OMPI_MAJOR_VERSION}")
-  string(REGEX MATCH "define OMPI_MINOR_VERSION * +([0-9]+)"
-        OMPI_MINOR_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
-  string(REGEX REPLACE "define OMPI_MINOR_VERSION * +([0-9]+)" "\\1"
-        OMPI_MINOR_VERSION "${OMPI_MINOR_VERSION}")
-  string(REGEX MATCH "define OMPI_RELEASE_VERSION * +([0-9]+)"
-        OMPI_PATCH_VERSION "${OMPI_VERSION_FILE_CONTENTS}")
-  string(REGEX REPLACE "define OMPI_RELEASE_VERSION * +([0-9]+)" "\\1"
-        OMPI_PATCH_VERSION "${OMPI_PATCH_VERSION}")
-  set(OMPI_VERSION ${OMPI_MAJOR_VERSION}.${OMPI_MINOR_VERSION}.${OMPI_PATCH_VERSION})
+found_PID_Configuration(openmpi FALSE)
 
-  #Check MPI less version
-  if (MPI_C_VERSION AND MPI_CXX_VERSION) # check if version is set (cmake version <3.10 is not set)
-    if(MPI_C_VERSION LESS_EQUAL MPI_CXX_VERSION)
-      set(MPI_VERSION ${MPI_C_VERSION})
-    else()
-      set(MPI_VERSION ${MPI_CXX_VERSION})
-    endif()
+# findMPI need to be use in a "non script" environment (try_compile function)
+# so we create a separate project (test_mpi) to generate usefull vars and extract them in a file (openmpi_config_vars.cmake)
+# and read this file in our context.
+
+set(path_test_mpi ${WORKSPACE_DIR}/configurations/openmpi/test_mpi/build/${CURRENT_PLATFORM})
+set(path_mpi_config_vars ${path_test_mpi}/openmpi_config_vars.cmake )
+
+if(EXISTS ${path_mpi_config_vars})#file already computed
+	include(${path_mpi_config_vars}) #just to check that same version is required
+	if(MPI_C_FOUND AND MPI_CXX_FOUND)#optimization only possible if openMPI has been found
+    set_openmpi_version()
+    if(NOT openmpi_version #no specific version to search for
+			OR openmpi_version VERSION_EQUAL OMPI_VERSION)# or same version required and already found no need to regenerate
+        set_openmpi_variables()
+      	found_PID_Configuration(openmpi TRUE)
+      return()#exit without regenerating (avoid regenerating between debug and release builds due to generated file timestamp change)
+			#also an optimization avoid launching again and again boost config for each package build in debug and release modes (which is widely used)
+		endif()
+	endif()
+endif()
+
+# execute separate project to extract datas
+if(NOT EXISTS ${path_test_mpi})
+	file(MAKE_DIRECTORY ${path_test_mpi})
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} ${WORKSPACE_DIR}/configurations/openmpi/test_mpi
+                WORKING_DIRECTORY ${path_test_mpi} OUTPUT_QUIET)
+
+# Extract datas from openmpi_config_vars.cmake
+if(EXISTS ${path_mpi_config_vars} )
+  include(${path_mpi_config_vars})
+else()
+  if(ADDITIONNAL_DEBUG_INFO)
+    message("[PID] WARNING : cannot execute tests for OpenMPI !")
   endif()
+  return()
+endif()
 
+if(MPI_C_FOUND AND MPI_CXX_FOUND)
+  set_openmpi_variables()
+  set_openmpi_version()
 	found_PID_Configuration(openmpi TRUE)
 else()
 	message("[PID] ERROR : cannot find OpenMPI library.")
