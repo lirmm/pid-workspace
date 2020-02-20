1f17a67a5402c42bc3efbe294d6cf9f1521e814e
Sonny Tarbouriech <sonny.tarbouriech@lirmm.fr>
2020-02-20 14:32:54 +0100

    Fixed reference to RKCL framework

--- share/cmake/references/ReferFrameworkrkcl-framework.cmake
+++ share/cmake/references/ReferFrameworkrkcl-framework.cmake
@@ -7,6 +7,6 @@ set(rkcl-framework_FRAMEWORK_SITE http://rkcl.lirmm.net/rkcl-framework CACHE INT
 set(rkcl-framework_FRAMEWORK_PROJECT_PAGE https://gite.lirmm.fr/rkcl/rkcl-framework CACHE INTERNAL "")
 set(rkcl-framework_FRAMEWORK_DESCRIPTION Framework for the Robot Kinematic Control Library (RKCL) CACHE INTERNAL "")
 set(rkcl-framework_FRAMEWORK_LICENSE CeCILL CACHE INTERNAL "")
-set(rkcl-framework_FRAMEWORK_ADDRESS git@gitlab.com:rkcl/rkcl-framework.git CACHE INTERNAL "")
+set(rkcl-framework_FRAMEWORK_ADDRESS git@gite.lirmm.fr:rkcl/rkcl-framework.git CACHE INTERNAL "")
 set(rkcl-framework_FRAMEWORK_AUTHORS_AND_INSTITUTIONS "_Sonny_Tarbouriech" CACHE INTERNAL "")
 set(rkcl-framework_FRAMEWORK_CATEGORIES "core;processor;driver;robot;application;solver" CACHE INTERNAL "")
